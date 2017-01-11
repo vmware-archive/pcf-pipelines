c776772fdf41e003be07a80c4cd6072eb084b8ae
John Calabrese <xchapter7x@gmail.com>
2017-01-10 17:32:47 -0800

    [#137374249] quick flesh out of initial tasks
    Signed-off-by: Merlin Glynn <mglynn@pivotal.io>

--- stage-product/task.sh
+++ stage-product/task.sh
@@ -4,28 +4,17 @@ function main() {
   local cwd
   cwd="${1}"
 
-  local opsman_dns
-  opsman_dns="$(cat "${cwd}/environment/name")"
-
   local version
   pushd "${cwd}/product"
     version="$(ls -1 *.pivotal | sed "s/"${PRODUCT}"-\(.*\).pivotal/\1/")"
   popd
 
-  if [[ -n $OPSMAN_URL_SUFFIX ]]; then
-    if [[ -n $USE_OPTIONAL_OPSMAN ]]; then
-      opsman_dns="pcf-optional.$opsman_dns.$OPSMAN_URL_SUFFIX"
-    else
-      opsman_dns="pcf.$opsman_dns.$OPSMAN_URL_SUFFIX"
-    fi
-  fi
-
-  om --target "https://${opsman_dns}" \
+  om --target "${OPSMAN_URI}" \
      --skip-ssl-validation \
      --username "${OPSMAN_USERNAME}" \
      --password "${OPSMAN_PASSWORD}" \
      stage-product \
-     --product-name "${PRODUCT}" \
+     --product-name "${PRODUCT_NAME}" \
      --product-version "${version}"
 }
 
