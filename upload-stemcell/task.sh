9d2fc0f8d091e5f9935634308ec12ca7ca9798a1
John Calabrese <xchapter7x@gmail.com>
2017-01-16 16:12:27 -0500

    [#137374301] adds new upload-stemcell task to bundle repo
    Signed-off-by: anwar chirakkattil <achirakkattil@pivotal.io>

---
+++ upload-stemcell/task.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -exu
+
+function main() {
+  local cwd
+  cwd="${1}"
+
+  local stemcell
+  stemcell="$(ls -1 "${cwd}"/stemcell/*.tgz)"
+
+  om --target ${OPSMAN_URI} \
+     --skip-ssl-validation \
+     --username "${OPSMAN_USERNAME}" \
+     --password "${OPSMAN_PASSWORD}" \
+     upload-stemcell \
+     --stemcell "${stemcell}"
+}
+
+main "${PWD}"
