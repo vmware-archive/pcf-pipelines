eb9ef150a5cf82ad82745da061ebca6e30f63527
John Calabrese <xchapter7x@gmail.com>
2017-01-16 17:28:45 -0500

    [#137374301] fix om-tool perms for execution
    Signed-off-by: anwar chirakkattil <achirakkattil@pivotal.io>

--- upload-stemcell/task.sh
+++ upload-stemcell/task.sh
@@ -7,6 +7,8 @@ function main() {
   local stemcell
   stemcell="$(ls -1 "${cwd}"/stemcell/*.tgz)"
 
+  chmod +x ./tool-om/om-linux
+
   ./tool-om/om-linux --target ${OPSMAN_URI} \
      --skip-ssl-validation \
      --username "${OPSMAN_USERNAME}" \
