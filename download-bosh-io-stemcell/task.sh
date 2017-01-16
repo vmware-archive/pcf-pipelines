d375d6da23668aa990db7751c98fe346eb2c0bef
John Calabrese <xchapter7x@gmail.com>
2017-01-16 17:24:16 -0500

    [#137374301] fixing om path and task scripts stemcell
    Signed-off-by: anwar chirakkattil <achirakkattil@pivotal.io>

--- download-bosh-io-stemcell/task.sh
+++ download-bosh-io-stemcell/task.sh
@@ -7,17 +7,10 @@ function main() {
   local product_file
   product_file="$(ls -1 ${cwd}/pivnet-product/*.pivotal)"
 
-  local product_name
-  if [[ -n ${PRODUCT} ]]; then
-    product_name=${PRODUCT}
-  else
-    product_name="cf"
-  fi
-
   ./concourse-tasks-bundle/download-bosh-io-stemcell/download-bosh-io-stemcell \
     --iaas-type "${IAAS_TYPE}" \
     --product-file "${product_file}" \
-    --product-name "${product_name}" \
+    --product-name "${PRODUCT}" \
     --download-dir "${cwd}/stemcell"
 }
 
--- upload-stemcell/task.sh
+++ upload-stemcell/task.sh
@@ -7,7 +7,7 @@ function main() {
   local stemcell
   stemcell="$(ls -1 "${cwd}"/stemcell/*.tgz)"
 
-  om --target ${OPSMAN_URI} \
+  ./tool-om/om-linux --target ${OPSMAN_URI} \
      --skip-ssl-validation \
      --username "${OPSMAN_USERNAME}" \
      --password "${OPSMAN_PASSWORD}" \
