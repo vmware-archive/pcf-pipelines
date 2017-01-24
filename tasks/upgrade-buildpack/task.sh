89798335bd68e33b756bcf5efed97d351d3c2af1
Kris Hicks <khicks@pivotal.io>
2017-01-24 15:02:28 -0800

    Whitespace

--- tasks/upgrade-buildpack/task.sh
+++ tasks/upgrade-buildpack/task.sh
@@ -5,8 +5,10 @@ function main() {
   chmod +x cf
 
   echo Using $(cf-cli/cf --version)
+
   cf api $PCF_API_URI --skip-ssl-validation
   cf login -u $PCF_USERNAME -p $PCF_PASSWORD -o $PCF_ORG -s $PCF_SPACE
+
   echo -n Creating buildpack ${BUILDPACK_NAME}...
   cf create-buildpack $BUILDPACK_NAME buildpack/*.zip 11 --enable
   echo done
