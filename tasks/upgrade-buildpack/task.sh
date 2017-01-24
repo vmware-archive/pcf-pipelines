cda42182ec6b7cd93375766cc9a5307d9ab0abef
Kris Hicks <khicks@pivotal.io>
2017-01-24 14:18:12 -0800

    Switch upgrade-buildpack pipelines to use this repo
    Also:
    
    * Make tasks/upgrade-buildpack/task.yml take a generic 'buildpack' input
    * Remove inlined task config
    * Add missing glob to tc buildpack pipeline

--- tasks/upgrade-buildpack/task.sh
+++ tasks/upgrade-buildpack/task.sh
@@ -9,7 +9,7 @@ function main() {
    echo "Upgrading buildpacks"
    cf api $PCF_API_URI --skip-ssl-validation
    cf login -u $PCF_USERNAME -p $PCF_PASSWORD -o $PCF_ORG -s $PCF_SPACE
-   cf create-buildpack $BUILDPACK_PREFIX-buildpack ./$BUILDPACK_PREFIX-buildpack-latest/*.zip 11 --enable
+   cf create-buildpack $BUILDPACK_NAME buildpack/*.zip 11 --enable
 }
 
 echo "Running import OpsMgr task..."
