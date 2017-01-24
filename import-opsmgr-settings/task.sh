b1cc374990f15c46c8a043f3dca45f57e77fb49c
Alex Shumilov <ashumilov@pivotal.io>
2017-01-24 16:49:03 -0500

    [#138190095] switch to use tool-om

--- import-opsmgr-settings/task.sh
+++ import-opsmgr-settings/task.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -eu
 
 function main() {
-  echo "Importing installation.zip file from OpsMgr"
-  curl "${OPSMAN_URI}/api/v0/installation_asset_collection" -X POST \
-    -F "installation[file]="./opsmanager-settings \
-    -F "passphrase=${OPSMAN_PASSPHRASE}" \
-    -k -vv
-  echo "Successfully uploaded " ./opsmanager-settings ". Return code [$?]."
+
+  chmod +x tool-om/om-linux
+  CMD_PATH="tool-om/om-linux"
+
+  local cwd
+  cwd="${1}"
+
+  ./${CMD_PATH} --target "${OPSMAN_URI}" \
+     --skip-ssl-validation \
+     --username "${OPSMAN_USERNAME}" \
+     --password "${OPSMAN_PASSWORD}" \
+     import-installation --installation "./opsmgr-settings"
+
+   echo "${OPSMAN_SETTINGS_FILENAME} Imported opsmgr-settings to ${OPSMAN_URI}."
 }
 
-echo "Running import OpsMgr task..."
 main "${PWD}"
