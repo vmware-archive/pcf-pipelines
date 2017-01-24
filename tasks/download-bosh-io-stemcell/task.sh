81f37375285e9934ac37329f591316703b99cd1c
Kris Hicks <khicks@pivotal.io>
2017-01-24 12:23:58 -0800

    Remove more Golang, more tasks use this repo
    The stemcell downloader now lives here:
    https://github.com/c0-ops/pcf-product-stemcell-downloader
    
    The upgrade-buildpack tasks were intentionally unchanged for now.

--- tasks/download-bosh-io-stemcell/task.sh
+++ tasks/download-bosh-io-stemcell/task.sh
@@ -7,7 +7,7 @@ function main() {
   local product_file
   product_file="$(ls -1 ${cwd}/pivnet-product/*.pivotal)"
 
-  ./concourse-tasks-bundle/download-bosh-io-stemcell/download-bosh-io-stemcell \
+  ./stemcell-downloader/stemcell-downloader-linux \
     --iaas-type "${IAAS_TYPE}" \
     --product-file "${product_file}" \
     --product-name "${PRODUCT}" \
