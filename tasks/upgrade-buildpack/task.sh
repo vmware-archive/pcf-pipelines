b4bdb9eb10ec0a023c4631537541f69c5f6b3341
Kris Hicks <khicks@pivotal.io>
2017-01-24 15:03:21 -0800

    Remove unnecessary main()
    Which was also being given the current working directory, which was
    unused.

--- tasks/upgrade-buildpack/task.sh
+++ tasks/upgrade-buildpack/task.sh
@@ -1,17 +1,13 @@
 #!/bin/bash -eu
 
-function main() {
-  tar -xvf cf-cli/*.tgz cf
-  chmod +x cf
+tar -xvf cf-cli/*.tgz cf
+chmod +x cf
 
-  echo Using $(cf-cli/cf --version)
+echo Using $(cf-cli/cf --version)
 
-  cf api $PCF_API_URI --skip-ssl-validation
-  cf login -u $PCF_USERNAME -p $PCF_PASSWORD -o $PCF_ORG -s $PCF_SPACE
+cf api $PCF_API_URI --skip-ssl-validation
+cf login -u $PCF_USERNAME -p $PCF_PASSWORD -o $PCF_ORG -s $PCF_SPACE
 
-  echo -n Creating buildpack ${BUILDPACK_NAME}...
-  cf create-buildpack $BUILDPACK_NAME buildpack/*.zip 11 --enable
-  echo done
-}
-
-main "${PWD}"
+echo -n Creating buildpack ${BUILDPACK_NAME}...
+cf create-buildpack $BUILDPACK_NAME buildpack/*.zip 11 --enable
+echo done
