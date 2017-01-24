adc646ab6625085bf60fcf77feba20f95b1f0511
Kris Hicks <khicks@pivotal.io>
2017-01-24 14:46:19 -0800

    Use resource for cf-cli

--- tasks/upgrade-buildpack/task.sh
+++ tasks/upgrade-buildpack/task.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -eu
 
 function main() {
-   echo "get cf cli"
-   wget -O cf-cli.deb "https://cli.run.pivotal.io/stable?release=debian64&source=github"
-   dpkg -i cf-cli.deb
-   cf --version
+  tar -xvf cf-cli/*.tgz cf
+  chmod +x cf
 
-   echo "Upgrading buildpacks"
-   cf api $PCF_API_URI --skip-ssl-validation
-   cf login -u $PCF_USERNAME -p $PCF_PASSWORD -o $PCF_ORG -s $PCF_SPACE
-   cf create-buildpack $BUILDPACK_NAME buildpack/*.zip 11 --enable
+  echo Using $(cf-cli/cf --version)
+  cf api $PCF_API_URI --skip-ssl-validation
+  cf login -u $PCF_USERNAME -p $PCF_PASSWORD -o $PCF_ORG -s $PCF_SPACE
+  echo -n Creating buildpack ${BUILDPACK_NAME}...
+  cf create-buildpack $BUILDPACK_NAME buildpack/*.zip 11 --enable
+  echo done
 }
 
 main "${PWD}"
