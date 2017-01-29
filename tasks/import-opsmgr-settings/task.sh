d3a5cacf8d713548a2a84774e4a3fe423e534f98
Alex Shumilov <ashumilov@pivotal.io>
2017-01-29 13:15:32 -0500

    om instead of curl on import

--- tasks/import-opsmgr-settings/task.sh
+++ tasks/import-opsmgr-settings/task.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -eu
 
 function main() {
-  echo "Wait for OpsMgr to be start"
-  sleep 3m
+    chmod +x tool-om/om-linux
+    CMD_PATH="tool-om/om-linux"
 
-  echo "Importing ${OPSMAN_SETTINGS_FILENAME} file from OpsMgr"
-  curl -vv -k "${OPSMAN_URI}/api/v0/installation_asset_collection" -X POST \
-   -F "installation[file]=@./opsmgr-settings/${OPSMAN_SETTINGS_FILENAME}" \
-   -F "passphrase=${OPSMAN_PASSWORD}"
-   echo "Successfully uploaded opsmgr-settings/${OPSMAN_SETTINGS_FILENAME}. Return code [$?]."
+    local cwd
+     cwd="${1}"
+
+     ./${CMD_PATH} --target "${OPSMAN_URI}" \
+        --skip-ssl-validation \
+        --username "${OPSMAN_USERNAME}" \
+        --password "${OPSMAN_PASSWORD}" \
+        import-installation --installation "./opsmgr-settings" --decryption-passphrase "${OPSMAN_PASSWORD}"
+
+     echo "${OPSMAN_SETTINGS_FILENAME} Imported opsmgr-settings to ${OPSMAN_URI}."
  }
 
  echo "Running import OpsMgr task..."
