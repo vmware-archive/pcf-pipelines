cacaa4d3213a605b1d43203182b1c8a88b413ad5
Alex Shumilov <ashumilov@pivotal.io>
2017-01-31 13:26:53 -0500

    bump up timeout while exporting settings

--- tasks/export-opsmgr-settings/task.sh
+++ tasks/export-opsmgr-settings/task.sh
@@ -12,7 +12,7 @@ function main() {
      --skip-ssl-validation \
      --username "${OPSMAN_USERNAME}" \
      --password "${OPSMAN_PASSWORD}" \
-     --request-timeout 3600 \
+     --request-timeout 6000 \
      export-installation --output-file "${OPSMAN_SETTINGS_FILENAME}"
 
    mv "${OPSMAN_SETTINGS_FILENAME}" ./opsmgr-settings
