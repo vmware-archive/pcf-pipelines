25418bb56856c24a84c2d131c9aa13a74bfade71
Alex Shumilov <ashumilov@pivotal.io>
2017-02-06 12:04:56 -0500

    [#138691961] use terraform template to push new aws instance of ops manager

--- tasks/deploy-opsman-vm-aws/task.sh
+++ tasks/deploy-opsman-vm-aws/task.sh
@@ -1,16 +1,17 @@
-#!/bin/bash -eu
+#!/bin/bash -u
 
 function main() {
 
-  # chmod +x terraform/terraform
-  CMD_PATH="terraform/terraform"
+local cwd
+cwd="${1}"
 
-  local cwd
-  cwd="${1}"
+gem install terraforming --no-ri --no-rdoc
+chmod +x terraform/terraform
+CMD_PATH="terraform/terraform"
 
 IAAS_CONFIGURATION=$(cat <<-EOF
 provider "aws" {
-  region = "${REGION}"
+  region = "${AWS_REGION}"
   access_key = "${AWS_ACCESS_KEY_ID}"
   secret_key = "${AWS_SECRET_ACCESS_KEY}"
 }
@@ -21,6 +22,7 @@ resource "aws_instance" "${AWS_INSTANCE}" {
   key_name = "${KEY_NAME}"
   subnet_id = "${SUBNET_ID}"
   associate_public_ip_address = "true"
+  instance_initiated_shutdown_behavior = "stop"
   vpc_security_group_ids = ["${SECURITY_GROUP}"]
   tags {
        Name = "${AWS_INSTANCE_NAME}"
@@ -40,9 +42,38 @@ EOF
 
   cat ./opsman_settings.tf
 
+  echo "Destroying current Ops Manager..."
+  terraforming ec2 --tfstate > ./terraform.tfstate
+  terraforming r53r --tfstate --merge ./terraform.tfstate --overwrite
+  RESOURCE_TO_DESTROY="aws_instance.${AWS_INSTANCE}"
+  ./${CMD_PATH} destroy -target=$RESOURCE_TO_DESTROY -force
+
+  rm ./terraform.tfstate
+
   echo "Creating Ops Manager instance in AWS..."
   ./${CMD_PATH} apply
-}
 
-echo "Running deploy of OpsMgr VM task..."
+# verify that ops manager started
+  started=false
+  timeout=$((SECONDS+${OPSMAN_TIMEOUT}))
+  export URL="https://${OPSMAN_SUBDOMAIN}.${OPSMAN_DOMAIN}"
+
+  echo "Starting Ops manager on ${URL}"
+
+  timeout=$((SECONDS+${OPSMAN_TIMEOUT}))
+  while [[ $started ]]; do
+    HTTP_OUTPUT=$(curl --write-out %{http_code} --silent -k --output /dev/null ${URL})
+    if [[ $HTTP_OUTPUT == *"302"* || $HTTP_OUTPUT == *"301"* ]]; then
+      echo "Site is started! $HTTP_OUTPUT"
+      break
+    else
+      echo "Ops manager is not running on ${URL}..."
+      if [[ $SECONDS -gt $timeout ]]; then
+        echo "Timed out waiting for ops manager site to start."
+        exit 1
+      fi
+    fi
+  done
+
+}
 main "${PWD}"
