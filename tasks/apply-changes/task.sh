9f5465b7f1410be5ca1e4777b477d1da79912b5b
Alex Shumilov <ashumilov@pivotal.io>
2017-02-07 11:51:03 -0500

    [#138691961] add polling to apply changes to wait for ops manager to start
    Signed-off-by: John Calabrese <xchapter7x@gmail.com>

--- tasks/apply-changes/task.sh
+++ tasks/apply-changes/task.sh
@@ -1,18 +1,31 @@
 #!/bin/bash -eu
 
 function main() {
-
+  echo "Applying changes on Ops manager @ ${OPSMAN_URI}"
   chmod +x tool-om/om-linux
   CMD_PATH="tool-om/om-linux"
+  TIMEOUT=$((SECONDS+${OPSMAN_TIMEOUT}))
+  set +e
+  while [[ true ]]; do
 
-  local cwd
-  cwd="${1}"
+    ./${CMD_PATH} --target "${OPSMAN_URI}" \
+       --skip-ssl-validation \
+       --username "${OPSMAN_USERNAME}" \
+       --password "${OPSMAN_PASSWORD}" \
+       apply-changes
 
-  ./${CMD_PATH} --target "${OPSMAN_URI}" \
-     --skip-ssl-validation \
-     --username "${OPSMAN_USERNAME}" \
-     --password "${OPSMAN_PASSWORD}" \
-     apply-changes
+    EXITCODE=$?
+
+    if [[ ${EXITCODE} -ne 0 && ${SECONDS} -gt ${TIMEOUT} ]]; then
+      echo "Timed out waiting for ops manager site to start."
+      exit 1
+    fi
+
+    if [[ ${EXITCODE} -eq 0 ]]; then
+      break
+    fi
+  done
+  set -e
 }
 
 main "${PWD}"
