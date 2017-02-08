5f28ede2fe76101f3e5bc64a2230097e37f02e3b
Kris Hicks <khicks@pivotal.io>
2017-02-08 09:04:32 -0800

    Remove download-bosh-io-stemcell task
    This is now rolled into upload-stemcell. There's no utility in having a
    separate job just for downloading a stemcell, and the following job to
    upload the stemcell may run on a different worker. In the event of a
    non-light stemcell this is unnecessary copying of the stemcell around.

--- tasks/upload-stemcell/task.sh
+++ tasks/upload-stemcell/task.sh
@@ -1,20 +1,28 @@
-#!/bin/bash -eu
+#!/bin/bash
 
-function main() {
-  local cwd
-  cwd="${1}"
+set -eu
 
-  local stemcell
-  stemcell="$(ls -1 "${cwd}"/stemcell/*.tgz)"
+root=$PWD
 
-  chmod +x ./tool-om/om-linux
+product_file="$(ls -1 ${root}/pivnet-product/*.pivotal)"
 
-  ./tool-om/om-linux --target ${OPSMAN_URI} \
-     --skip-ssl-validation \
-     --username "${OPSMAN_USERNAME}" \
-     --password "${OPSMAN_PASSWORD}" \
-     upload-stemcell \
-     --stemcell "${stemcell}"
-}
+chmod +x stemcell-downloader/stemcell-downloader-linux
 
-main "${PWD}"
+mkdir -p "${root}/stemcell"
+
+./stemcell-downloader/stemcell-downloader-linux \
+  --iaas-type "${IAAS_TYPE}" \
+  --product-file "${product_file}" \
+  --product-name "${PRODUCT}" \
+  --download-dir "${root}/stemcell"
+
+stemcell="$(ls -1 "${root}"/stemcell/*.tgz)"
+
+chmod +x ./tool-om/om-linux
+
+./tool-om/om-linux --target ${OPSMAN_URI} \
+  --skip-ssl-validation \
+  --username "${OPSMAN_USERNAME}" \
+  --password "${OPSMAN_PASSWORD}" \
+  upload-stemcell \
+  --stemcell "${stemcell}"
