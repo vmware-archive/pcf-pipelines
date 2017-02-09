bbcd1a7d6130959d421748b79ac4a01e3e49bee3
Mark Alston <malston@pivotal.io>
2017-02-08 22:47:12 -0700

    Fix path to om tool

--- tasks/import-opsmgr-settings/task.sh
+++ tasks/import-opsmgr-settings/task.sh
@@ -5,20 +5,20 @@ function main() {
   cwd="${1}"
 
   chmod +x tool-om/om-linux
-  local om
-  om="tool-om/om-linux"
+  local om="tool-om/om-linux"
 
-  printf 'Waiting for opsman to come up'
+  printf "Waiting for %s to come up" "$OPSMAN_URI"
   until $(curl --output /dev/null --silent --head --fail -k ${OPSMAN_URI}); do
     printf '.'
     sleep 5
   done
+  printf '\n'
 
-  om --target "${OPSMAN_URI}" \
-     --skip-ssl-validation \
-     import-installation \
-     --installation "${cwd}/opsmgr-settings/${OPSMAN_SETTINGS_FILENAME}" \
-     --decryption-passphrase "${OPSMAN_PASSPHRASE}"
+  $om --target "${OPSMAN_URI}" \
+      --skip-ssl-validation \
+      import-installation \
+      --installation "${cwd}/opsmgr-settings/${OPSMAN_SETTINGS_FILENAME}" \
+      --decryption-passphrase "${OPSMAN_PASSPHRASE}"
  }
 
  main "${PWD}"
