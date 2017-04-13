f8052b15c40a0d9c066d46404b065a58d648b6d8
Administrator <admin@example.com>
2017-04-13 14:59:45 -0500

    Adds my specific stuff

---
+++ upgrade-ert/ert-fly.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+fly -t ci login -u concourse -p changeme -c http://10.193.65.102:8080/
+
+fly -t ci set-pipeline --pipeline upgrade-ert --config pipeline.yml --load-vars-from .params.yml
+fly -t ci unpause-pipeline --pipeline upgrade-ert
