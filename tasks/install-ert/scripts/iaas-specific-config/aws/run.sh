3d8c755a12ca399eff5ee4de0c3432b1e070e833
Kris Hicks <khicks@pivotal.io>
2017-05-15 10:38:47 -0700

    Remove perl from aws/run.sh

--- tasks/install-ert/scripts/iaas-specific-config/aws/run.sh
+++ tasks/install-ert/scripts/iaas-specific-config/aws/run.sh
@@ -13,14 +13,11 @@ cd pcfawsops-terraform-state-get
   export AWS_SECRET_ACCESS_KEY=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^secret | awk '{print $3}'`
 cd -
 
-json_file="json_file/ert.json"
-
-# Set JSON Config Template and inster Concourse Parameter Values
-
-perl -pi -e "s/{{db_host}}/${db_host}/g" ${json_file}
-
-perl -pi -e "s/{{aws_access_key}}/${AWS_ACCESS_KEY_ID}/g" ${json_file}
-perl -pi -e "s%{{aws_secret_key}}%${AWS_SECRET_ACCESS_KEY}%g" ${json_file}
-perl -pi -e "s/{{aws_region}}/${region}/g" ${json_file}
-perl -pi -e "s%{{s3_endpoint}}%${S3_ENDPOINT}%g" ${json_file}
-perl -pi -e "s/{{syslog_host}}/${SYSLOG_HOST}/g" ${json_file}
+sed -i \
+  -e "s/{{db_host}}/${db_host}/g" \
+  -e "s/{{aws_access_key}}/${AWS_ACCESS_KEY_ID}/g" \
+  -e "s%{{aws_secret_key}}%${AWS_SECRET_ACCESS_KEY}%g" \
+  -e "s/{{aws_region}}/${region}/g" \
+  -e "s%{{s3_endpoint}}%${S3_ENDPOINT}%g" \
+  -e "s/{{syslog_host}}/${SYSLOG_HOST}/g" \
+  json_file/ert.json
