72d5e5bb145e392a74f43b7b64c5a981d97c87d7
Kris Hicks <khicks@pivotal.io>
2017-05-15 14:51:23 -0700

    Remove gcloud from upload-opsman.sh

--- tasks/install-pcf-gcp/upload-opsman.sh
+++ tasks/install-pcf-gcp/upload-opsman.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -eu
 
-google_creds_json=$(mktemp)
-echo $GCP_SERVICE_ACCOUNT_KEY > $google_creds_json
-gcloud auth activate-service-account --key-file $google_creds_json
-gcloud config set project $GCP_PROJECT_ID
-gcloud config set compute/region $GCP_REGION
+export GOOGLE_CREDENTIALS=${GCP_SERVICE_ACCOUNT_KEY}
+export GOOGLE_PROJECT=${GCP_PROJECT_ID}
+export GOOGLE_REGION=${GCP_REGION}
 
 # us: ops-manager-us/pcf-gcp-1.9.2.tar.gz -> ops-manager-us/pcf-gcp-1.9.2.tar.gz
 pcf_opsman_bucket_path=$(grep -i 'us:.*.tar.gz' pivnet-opsmgr/*GCP.yml | cut -d' ' -f2)
@@ -13,11 +11,20 @@ pcf_opsman_bucket_path=$(grep -i 'us:.*.tar.gz' pivnet-opsmgr/*GCP.yml | cut -d'
 # ops-manager-us/pcf-gcp-1.9.2.tar.gz -> opsman-pcf-gcp-1-9-2
 pcf_opsman_image_name=$(echo $pcf_opsman_bucket_path | sed 's%.*/\(.*\).tar.gz%opsman-\1%' | sed 's/\./-/g')
 
-if [[ -z $(gcloud compute images list | grep $pcf_opsman_image_name) ]]; then
-  echo "creating image ${pcf_opsman_image_name}"
-  gcloud compute images create $pcf_opsman_image_name \
-    --family pcf-opsman \
-    --source-uri "gs://${pcf_opsman_bucket_path}"
-else
-  echo "image ${pcf_opsman_image_name} already exists"
-fi
+mkdir terraform && cd terraform
+
+cat > image.tf <<EOF
+resource "google_compute_image" "ops-mgr" {
+  name = "$pcf_opsman_image_name"
+  project = "$GCP_PROJECT_ID"
+  family = "pcf-opsman"
+  description = "Pivotal Cloud Foundry Operations Manager"
+
+  raw_disk {
+    source = "gs://${pcf_opsman_bucket_path}"
+  }
+}
+EOF
+
+set +e
+terraform apply
