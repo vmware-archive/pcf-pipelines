92b795da734d164e11946ff10e50c0cea706ae1e
Kris Hicks <khicks@pivotal.io>
2017-05-15 10:40:20 -0700

    Use % as sed delimiter in aws/run.sh

--- tasks/install-ert/scripts/iaas-specific-config/aws/run.sh
+++ tasks/install-ert/scripts/iaas-specific-config/aws/run.sh
@@ -14,10 +14,10 @@ cd pcfawsops-terraform-state-get
 cd -
 
 sed -i \
-  -e "s/{{db_host}}/${db_host}/g" \
-  -e "s/{{aws_access_key}}/${AWS_ACCESS_KEY_ID}/g" \
+  -e "s%{{db_host}}%${db_host}%g" \
+  -e "s%{{aws_access_key}}%${AWS_ACCESS_KEY_ID}%g" \
   -e "s%{{aws_secret_key}}%${AWS_SECRET_ACCESS_KEY}%g" \
-  -e "s/{{aws_region}}/${region}/g" \
+  -e "s%{{aws_region}}%${region}%g" \
   -e "s%{{s3_endpoint}}%${S3_ENDPOINT}%g" \
-  -e "s/{{syslog_host}}/${SYSLOG_HOST}/g" \
+  -e "s%{{syslog_host}}%${SYSLOG_HOST}%g" \
   json_file/ert.json
