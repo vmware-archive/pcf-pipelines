57050982ca781e9be010d9396f7b9aba959104e4
Kris Hicks <khicks@pivotal.io>
2017-05-15 11:59:11 -0700

    Remove perl from configure-director.sh

--- tasks/install-pcf-aws/config-director.sh
+++ tasks/install-pcf-aws/config-director.sh
@@ -23,56 +23,49 @@ json_file="${json_file_path}/opsman.json"
 
 cp ${json_file_template} ${json_file}
 
-export S3_ESCAPED=${S3_ENDPOINT//\//\\/}
-
-perl -pi -e "s/{{aws_vpc_id}}/${vpc_id}/g" ${json_file}
-perl -pi -e "s/{{aws_sg_id}}/${pcf_security_group}/g" ${json_file}
-perl -pi -e "s/{{aws_keypair_name}}/${AWS_KEY_NAME}/g" ${json_file}
-perl -pi -e "s/{{aws_region}}/${AWS_REGION}/g" ${json_file}
-perl -pi -e "s/{{s3_endpoint}}/${S3_ESCAPED}/g" ${json_file}
-perl -pi -e "s/{{s3_bucket}}/${s3_pcf_bosh}/g" ${json_file}
-perl -pi -e "s/{{db_host}}/${db_host}/g" ${json_file}
-perl -pi -e "s/{{db_user}}/${db_username}/g" ${json_file}
-perl -pi -e "s/{{rds_database}}/${db_database}/g" ${json_file}
-perl -pi -e "s/{{aws_az1}}/${az1}/g" ${json_file}
-perl -pi -e "s/{{aws_az2}}/${az2}/g" ${json_file}
-perl -pi -e "s/{{aws_az3}}/${az3}/g" ${json_file}
-perl -pi -e "s/{{vpc_dns}}/${dns}/g" ${json_file}
-
-perl -pi -e "s/{{deployment_subnet_1}}/${ert_subnet_id_az1}/g" ${json_file}
-perl -pi -e "s|{{deployment_subnet_1_cidr}}|${ert_subnet_cidr_az1}|g" ${json_file}
-perl -pi -e "s/{{deployment_subnet_1_reserved}}/${ert_subnet_reserved_ranges_z1}/g" ${json_file}
-perl -pi -e "s/{{deployment_subnet_1_gw}}/${ert_subnet_gw_az1}/g" ${json_file}
-
-perl -pi -e "s/{{deployment_subnet_2}}/${ert_subnet_id_az2}/g" ${json_file}
-perl -pi -e "s|{{deployment_subnet_2_cidr}}|${ert_subnet_cidr_az2}|g" ${json_file}
-perl -pi -e "s/{{deployment_subnet_2_reserved}}/${ert_subnet_reserved_ranges_z2}/g" ${json_file}
-perl -pi -e "s/{{deployment_subnet_2_gw}}/${ert_subnet_gw_az2}/g" ${json_file}
-
-perl -pi -e "s/{{deployment_subnet_3}}/${ert_subnet_id_az3}/g" ${json_file}
-perl -pi -e "s|{{deployment_subnet_3_cidr}}|${ert_subnet_cidr_az3}|g" ${json_file}
-perl -pi -e "s/{{deployment_subnet_3_reserved}}/${ert_subnet_reserved_ranges_z3}/g" ${json_file}
-perl -pi -e "s/{{deployment_subnet_3_gw}}/${ert_subnet_gw_az3}/g" ${json_file}
-
-perl -pi -e "s/{{services_subnet_1}}/${services_subnet_id_az1}/g" ${json_file}
-perl -pi -e "s|{{services_subnet_1_cidr}}|${services_subnet_cidr_az1}|g" ${json_file}
-perl -pi -e "s/{{services_subnet_1_reserved}}/${services_subnet_reserved_ranges_z1}/g" ${json_file}
-perl -pi -e "s/{{services_subnet_1_gw}}/${services_subnet_gw_az1}/g" ${json_file}
-
-perl -pi -e "s/{{services_subnet_2}}/${services_subnet_id_az2}/g" ${json_file}
-perl -pi -e "s|{{services_subnet_2_cidr}}|${services_subnet_cidr_az2}|g" ${json_file}
-perl -pi -e "s/{{services_subnet_2_reserved}}/${services_subnet_reserved_ranges_z2}/g" ${json_file}
-perl -pi -e "s/{{services_subnet_2_gw}}/${services_subnet_gw_az2}/g" ${json_file}
-
-perl -pi -e "s/{{services_subnet_3}}/${services_subnet_id_az3}/g" ${json_file}
-perl -pi -e "s|{{services_subnet_3_cidr}}|${services_subnet_cidr_az3}|g" ${json_file}
-perl -pi -e "s/{{services_subnet_3_reserved}}/${services_subnet_reserved_ranges_z3}/g" ${json_file}
-perl -pi -e "s/{{services_subnet_3_gw}}/${services_subnet_gw_az3}/g" ${json_file}
-
-perl -pi -e "s/{{infra_subnet}}/${infra_subnet_id_az1}/g" ${json_file}
-perl -pi -e "s|{{infra_subnet_cidr}}|${infra_subnet_cidr_az1}|g" ${json_file}
-perl -pi -e "s/{{infra_subnet_reserved}}/${infra_subnet_reserved_ranges_z1}/g" ${json_file}
-perl -pi -e "s/{{infra_subnet_gw}}/${infra_subnet_gw_az1}/g" ${json_file}
+sed -i \
+  -e "s/{{aws_vpc_id}}/${vpc_id}/g" \
+  -e "s/{{aws_sg_id}}/${pcf_security_group}/g" \
+  -e "s/{{aws_keypair_name}}/${AWS_KEY_NAME}/g" \
+  -e "s/{{aws_region}}/${AWS_REGION}/g" \
+  -e "s%{{s3_endpoint}}%${S3_ENDPOINT}%g" \
+  -e "s/{{s3_bucket}}/${s3_pcf_bosh}/g" \
+  -e "s/{{db_host}}/${db_host}/g" \
+  -e "s/{{db_user}}/${db_username}/g" \
+  -e "s/{{rds_database}}/${db_database}/g" \
+  -e "s/{{aws_az1}}/${az1}/g" \
+  -e "s/{{aws_az2}}/${az2}/g" \
+  -e "s/{{aws_az3}}/${az3}/g" \
+  -e "s/{{vpc_dns}}/${dns}/g" \
+  -e "s/{{deployment_subnet_1}}/${ert_subnet_id_az1}/g" \
+  -e "s|{{deployment_subnet_1_cidr}}|${ert_subnet_cidr_az1}|g" \
+  -e "s/{{deployment_subnet_1_reserved}}/${ert_subnet_reserved_ranges_z1}/g" \
+  -e "s/{{deployment_subnet_1_gw}}/${ert_subnet_gw_az1}/g" \
+  -e "s/{{deployment_subnet_2}}/${ert_subnet_id_az2}/g" \
+  -e "s|{{deployment_subnet_2_cidr}}|${ert_subnet_cidr_az2}|g" \
+  -e "s/{{deployment_subnet_2_reserved}}/${ert_subnet_reserved_ranges_z2}/g" \
+  -e "s/{{deployment_subnet_2_gw}}/${ert_subnet_gw_az2}/g" \
+  -e "s/{{deployment_subnet_3}}/${ert_subnet_id_az3}/g" \
+  -e "s|{{deployment_subnet_3_cidr}}|${ert_subnet_cidr_az3}|g" \
+  -e "s/{{deployment_subnet_3_reserved}}/${ert_subnet_reserved_ranges_z3}/g" \
+  -e "s/{{deployment_subnet_3_gw}}/${ert_subnet_gw_az3}/g" \
+  -e "s/{{services_subnet_1}}/${services_subnet_id_az1}/g" \
+  -e "s|{{services_subnet_1_cidr}}|${services_subnet_cidr_az1}|g" \
+  -e "s/{{services_subnet_1_reserved}}/${services_subnet_reserved_ranges_z1}/g" \
+  -e "s/{{services_subnet_1_gw}}/${services_subnet_gw_az1}/g" \
+  -e "s/{{services_subnet_2}}/${services_subnet_id_az2}/g" \
+  -e "s|{{services_subnet_2_cidr}}|${services_subnet_cidr_az2}|g" \
+  -e "s/{{services_subnet_2_reserved}}/${services_subnet_reserved_ranges_z2}/g" \
+  -e "s/{{services_subnet_2_gw}}/${services_subnet_gw_az2}/g" \
+  -e "s/{{services_subnet_3}}/${services_subnet_id_az3}/g" \
+  -e "s|{{services_subnet_3_cidr}}|${services_subnet_cidr_az3}|g" \
+  -e "s/{{services_subnet_3_reserved}}/${services_subnet_reserved_ranges_z3}/g" \
+  -e "s/{{services_subnet_3_gw}}/${services_subnet_gw_az3}/g" \
+  -e "s/{{infra_subnet}}/${infra_subnet_id_az1}/g" \
+  -e "s|{{infra_subnet_cidr}}|${infra_subnet_cidr_az1}|g" \
+  -e "s/{{infra_subnet_reserved}}/${infra_subnet_reserved_ranges_z1}/g" \
+  -e "s/{{infra_subnet_gw}}/${infra_subnet_gw_az1}/g" \
+  $json_file
 
 echo "=============================================================================================="
 echo "Configuring Director @ https://opsman.$ERT_DOMAIN ..."
