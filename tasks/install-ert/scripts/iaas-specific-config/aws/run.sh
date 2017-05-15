2b9011c2cfb0f1caf0c1ac8dc78aebd199bf7349
Kris Hicks <khicks@pivotal.io>
2017-05-15 11:05:16 -0700

    Don't export entire terraform output vars
    Only db_host and aws_region are needed from outputs.

--- tasks/install-ert/scripts/iaas-specific-config/aws/run.sh
+++ tasks/install-ert/scripts/iaas-specific-config/aws/run.sh
@@ -3,21 +3,24 @@ set -e
 
 export PATH=$PATH:/opt/terraform
 
-cd pcfawsops-terraform-state-get
-  while read -r line
-  do
-    `echo "$line" | awk '{print "export "$1"="$3}'`
-  done < <(terraform output)
+db_host=''
+aws_region=''
+aws_access_key=''
+aws_secret_key=''
 
-  export AWS_ACCESS_KEY_ID=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^id | awk '{print $3}'`
-  export AWS_SECRET_ACCESS_KEY=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^secret | awk '{print $3}'`
+cd pcfawsops-terraform-state-get
+  output_json=$(terraform output --json)
+  db_host=$(echo $output_json | jq --raw-output '.db_host.value')
+  aws_region=$(echo $output_json | jq --raw-output '.region.value')
+  aws_access_key=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^id | awk '{print $3}'`
+  aws_secret_key=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^secret | awk '{print $3}'`
 cd -
 
 sed -i \
   -e "s%{{db_host}}%${db_host}%g" \
-  -e "s%{{aws_access_key}}%${AWS_ACCESS_KEY_ID}%g" \
-  -e "s%{{aws_secret_key}}%${AWS_SECRET_ACCESS_KEY}%g" \
-  -e "s%{{aws_region}}%${region}%g" \
+  -e "s%{{aws_access_key}}%${aws_access_key}%g" \
+  -e "s%{{aws_secret_key}}%${aws_secret_key}%g" \
+  -e "s%{{aws_region}}%${aws_region}%g" \
   -e "s%{{s3_endpoint}}%${S3_ENDPOINT}%g" \
   -e "s%{{syslog_host}}%${SYSLOG_HOST}%g" \
   json_file/ert.json
