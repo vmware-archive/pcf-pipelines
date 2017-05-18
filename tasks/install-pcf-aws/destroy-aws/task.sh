1a99234f3d199d352094395426cd89cb5be7cbd2
Kris Hicks <khicks@pivotal.io>
2017-05-18 15:46:27 -0700

    Update destroy-aws output
    * Use -state-out and -state for terraform state
    * Put updated state to terraform-state resource

--- tasks/install-pcf-aws/destroy-aws/task.sh
+++ tasks/install-pcf-aws/destroy-aws/task.sh
@@ -2,14 +2,15 @@
 set -e
 unzip terraform-zip/terraform.zip
 mv terraform /usr/local/bin
-CWD=$(pwd)
+root=$(pwd)
 cd pcf-pipelines/tasks/install-pcf-aws/terraform/
-cp $CWD/terraform-state/terraform.tfstate .
 
 export AWS_ACCESS_KEY_ID=${TF_VAR_aws_access_key}
 export AWS_SECRET_ACCESS_KEY=${TF_VAR_aws_secret_key}
 export AWS_DEFAULT_REGION=${TF_VAR_aws_region}
-export VPC_ID=`terraform state show aws_vpc.PcfVpc | grep ^id | awk '{print $3}'`
+export VPC_ID=$(
+  terraform state show -state $root/terraform-state/terraform.tfstate aws_vpc.PcfVpc | grep ^id | awk '{print $3}'
+)
 
 #Clean AWS instances
 python get-pip/get-pip.py
@@ -23,10 +24,8 @@ then
   aws ec2 wait instance-terminated --instance-ids $instances
 fi
 
-#Destroy the plan
-terraform plan
 set +e
-terraform destroy -force
-
-cd $CWD/terraform-state
-touch terraform.tfstate
+terraform destroy \
+  -force \
+  -state $root/terraform-state/terraform.tfstate \
+  -state-out $root/terraform-state-output/terraform.tfstate
