b7ed2a72068041dfaf1fd63ec35835c362ec115a
Kris Hicks <khicks@pivotal.io>
2017-06-02 10:42:29 -0700

    Update aws/config-director/task.sh

--- tasks/install-pcf-aws/config-director/task.sh
+++ tasks/install-pcf-aws/config-director/task.sh
@@ -1,72 +1,195 @@
 #!/bin/bash
 set -ex
 
-root=$(pwd)
-cd pcf-pipelines/tasks/install-pcf-aws/terraform/
-cp $root/terraform-state/terraform.tfstate .
+aws_access_key_id=`terraform state show -state terraform-state/terraform.tfstate aws_iam_access_key.pcf_iam_user_access_key | grep ^id | awk '{print $3}'`
+aws_secret_access_key=`terraform state show -state terraform-state/terraform.tfstate aws_iam_access_key.pcf_iam_user_access_key | grep ^secret | awk '{print $3}'`
+rds_password=`terraform state show -state terraform-state/terraform.tfstate aws_db_instance.pcf_rds | grep ^password | awk '{print $3}'`
 
 while read -r line
 do
   `echo "$line" | awk '{print "export "$1"="$3}'`
-done < <(terraform output -state *.tfstate)
+done < <(terraform output -state terraform-state/terraform.tfstate)
 
-export AWS_ACCESS_KEY_ID=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^id | awk '{print $3}'`
-export AWS_SECRET_ACCESS_KEY=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^secret | awk '{print $3}'`
-export RDS_PASSWORD=`terraform state show aws_db_instance.pcf_rds | grep ^password | awk '{print $3}'`
 
-cd $root
+set +e
+read -r -d '' iaas_configuration <<EOF
+{
+  "access_key_id": "$aws_access_key_id",
+  "secret_access_key": "$aws_secret_access_key",
+  "vpc_id": "$vpc_id",
+  "security_group": "$pcf_security_group",
+  "key_pair_name": "$AWS_KEY_NAME",
+  "ssh_private_key": "",
+  "region": "$AWS_REGION",
+  "encrypted": false
+}
+EOF
 
-sed -e "s/{{aws_vpc_id}}/${vpc_id}/g" \
-  -e "s/{{aws_sg_id}}/${pcf_security_group}/g" \
-  -e "s/{{aws_keypair_name}}/${AWS_KEY_NAME}/g" \
-  -e "s/{{aws_region}}/${AWS_REGION}/g" \
-  -e "s%{{s3_endpoint}}%${S3_ENDPOINT}%g" \
-  -e "s/{{s3_bucket}}/${s3_pcf_bosh}/g" \
-  -e "s/{{db_host}}/${db_host}/g" \
-  -e "s/{{db_user}}/${db_username}/g" \
-  -e "s/{{rds_database}}/${db_database}/g" \
-  -e "s/{{aws_az1}}/${az1}/g" \
-  -e "s/{{aws_az2}}/${az2}/g" \
-  -e "s/{{aws_az3}}/${az3}/g" \
-  -e "s/{{vpc_dns}}/${dns}/g" \
-  -e "s/{{deployment_subnet_1}}/${ert_subnet_id_az1}/g" \
-  -e "s|{{deployment_subnet_1_cidr}}|${ert_subnet_cidr_az1}|g" \
-  -e "s/{{deployment_subnet_1_reserved}}/${ert_subnet_reserved_ranges_z1}/g" \
-  -e "s/{{deployment_subnet_1_gw}}/${ert_subnet_gw_az1}/g" \
-  -e "s/{{deployment_subnet_2}}/${ert_subnet_id_az2}/g" \
-  -e "s|{{deployment_subnet_2_cidr}}|${ert_subnet_cidr_az2}|g" \
-  -e "s/{{deployment_subnet_2_reserved}}/${ert_subnet_reserved_ranges_z2}/g" \
-  -e "s/{{deployment_subnet_2_gw}}/${ert_subnet_gw_az2}/g" \
-  -e "s/{{deployment_subnet_3}}/${ert_subnet_id_az3}/g" \
-  -e "s|{{deployment_subnet_3_cidr}}|${ert_subnet_cidr_az3}|g" \
-  -e "s/{{deployment_subnet_3_reserved}}/${ert_subnet_reserved_ranges_z3}/g" \
-  -e "s/{{deployment_subnet_3_gw}}/${ert_subnet_gw_az3}/g" \
-  -e "s/{{services_subnet_1}}/${services_subnet_id_az1}/g" \
-  -e "s|{{services_subnet_1_cidr}}|${services_subnet_cidr_az1}|g" \
-  -e "s/{{services_subnet_1_reserved}}/${services_subnet_reserved_ranges_z1}/g" \
-  -e "s/{{services_subnet_1_gw}}/${services_subnet_gw_az1}/g" \
-  -e "s/{{services_subnet_2}}/${services_subnet_id_az2}/g" \
-  -e "s|{{services_subnet_2_cidr}}|${services_subnet_cidr_az2}|g" \
-  -e "s/{{services_subnet_2_reserved}}/${services_subnet_reserved_ranges_z2}/g" \
-  -e "s/{{services_subnet_2_gw}}/${services_subnet_gw_az2}/g" \
-  -e "s/{{services_subnet_3}}/${services_subnet_id_az3}/g" \
-  -e "s|{{services_subnet_3_cidr}}|${services_subnet_cidr_az3}|g" \
-  -e "s/{{services_subnet_3_reserved}}/${services_subnet_reserved_ranges_z3}/g" \
-  -e "s/{{services_subnet_3_gw}}/${services_subnet_gw_az3}/g" \
-  -e "s/{{infra_subnet}}/${infra_subnet_id_az1}/g" \
-  -e "s|{{infra_subnet_cidr}}|${infra_subnet_cidr_az1}|g" \
-  -e "s/{{infra_subnet_reserved}}/${infra_subnet_reserved_ranges_z1}/g" \
-  -e "s/{{infra_subnet_gw}}/${infra_subnet_gw_az1}/g" \
-  pcf-pipelines/tasks/install-pcf-aws/opsman-template.json > opsman.json
+read -r -d '' director_configuration <<EOF
+{
+  "ntp_servers_string": "0.amazon.pool.ntp.org,1.amazon.pool.ntp.org,2.amazon.pool.ntp.org,3.amazon.pool.ntp.org",
+  "resurrector_enabled": true,
+  "max_threads": 30,
+  "database_type": "external",
+  "external_database_options": {
+    "host": "$db_host",
+    "port": 3306,
+    "user": "$db_username",
+    "password": "$rds_password",
+    "database": "$db_database"
+  },
+  "blobstore_type": "s3",
+  "s3_blobstore_options": {
+    "endpoint": "$S3_ENDPOINT",
+    "bucket_name": "$s3_pcf_bosh",
+    "access_key": "$aws_access_key_id",
+    "secret_key": "$aws_secret_access_key",
+    "signature_version": "2",
+    "region": "$AWS_REGION"
+  }
+}
+EOF
 
-echo "=============================================================================================="
-echo "Configuring Director @ https://opsman.$ERT_DOMAIN ..."
-cat opsman.json
+read -r -d '' az_configuration <<EOF
+{
+  "availability_zones": [
+    { "name": "$az1" },
+    { "name": "$az2" },
+    { "name": "$az3" }
+  ]
+}
+EOF
 
-echo "=============================================================================================="
+read -r -d '' networks_configuration <<EOF
+{
+  "icmp_checks_enabled": false,
+  "networks": [
+    {
+      "name": "deployment",
+      "service_network": false,
+      "subnets": [
+        {
+          "iaas_identifier": "$ert_subnet_id_az1",
+          "cidr": "$ert_subnet_cidr_az1",
+          "reserved_ip_ranges": "$ert_subnet_reserved_ranges_z1",
+          "dns": "$dns",
+          "gateway": "$ert_subnet_gw_az1",
+          "availability_zones": ["$az1"]
+        },
+        {
+          "iaas_identifier": "$ert_subnet_id_az2",
+          "cidr": "$ert_subnet_cidr_az2",
+          "reserved_ip_ranges": "$ert_subnet_reserved_ranges_z2",
+          "dns": "$dns",
+          "gateway": "$ert_subnet_gw_az2",
+          "availability_zones": ["$az2"]
+        },
+        {
+          "iaas_identifier": "$ert_subnet_id_az3",
+          "cidr": "$ert_subnet_cidr_az3",
+          "reserved_ip_ranges": "$ert_subnet_reserved_ranges_z3",
+          "dns": "$dns",
+          "gateway": "$ert_subnet_gw_az3",
+          "availability_zones": ["$az3"]
+        }
+      ]
+    },
+    {
+      "name": "infrastructure",
+      "service_network": false,
+      "subnets": [
+        {
+          "iaas_identifier": "$infra_subnet_id_az1",
+          "cidr": "$infra_subnet_cidr_az1",
+          "reserved_ip_ranges": "$infra_subnet_reserved_ranges_z1",
+          "dns": "$dns",
+          "gateway": "$infra_subnet_gw_az1",
+          "availability_zones": ["$az1"]
+        }
+      ]
+    },
+    {
+      "name": "services",
+      "service_network": false,
+      "subnets": [
+        {
+          "iaas_identifier": "$services_subnet_id_az1",
+          "cidr": "$services_subnet_cidr_az1",
+          "reserved_ip_ranges": "$services_subnet_reserved_ranges_z1",
+          "dns": "$dns",
+          "gateway": "$services_subnet_gw_az1",
+          "availability_zones": ["$az1"]
+        },
+        {
+          "iaas_identifier": "$services_subnet_id_az2",
+          "cidr": "$services_subnet_cidr_az2",
+          "reserved_ip_ranges": "$services_subnet_reserved_ranges_z2",
+          "dns": "$dns",
+          "gateway": "$services_subnet_gw_az2",
+          "availability_zones": ["$az2"]
+        },
+        {
+          "iaas_identifier": "$services_subnet_id_az3",
+          "cidr": "$services_subnet_cidr_az3",
+          "reserved_ip_ranges": "$services_subnet_reserved_ranges_z3",
+          "dns": "$dns",
+          "gateway": "$services_subnet_gw_az3",
+          "availability_zones": ["$az3"]
+        }
+      ]
+    }
+  ]
+}
+EOF
 
-om-linux-beta -t https://opsman.$ERT_DOMAIN -u "$OPSMAN_USER" -p "$OPSMAN_PASSWORD" -k \
-  aws -a $AWS_ACCESS_KEY_ID \
-  -s $AWS_SECRET_ACCESS_KEY \
-  -d $RDS_PASSWORD \
-  -p "$PEM" -c "$(cat opsman.json)"
+read -r -d '' network_assignment <<EOF
+{
+  "singleton_availability_zone": "$az1",
+  "network": "infrastructure"
+}
+EOF
+
+read -r -d '' security_configuration <<EOF
+{
+  "trusted_certificates": "",
+  "vm_password_type": "generate"
+}
+EOF
+set -e
+
+iaas_configuration=$(
+  echo "$iaas_configuration" |
+  jq --arg ssh_private_key "$PEM" '.ssh_private_key = $ssh_private_key'
+)
+
+security_configuration=$(
+  echo "$security_configuration" |
+  jq --arg certs "$TRUSTED_CERTIFICATES" '.trusted_certificates = $certs'
+)
+
+jsons=(
+  "$iaas_configuration"
+  "$director_configuration"
+  "$az_configuration"
+  "$networks_configuration"
+  "$network_assignment"
+  "$security_configuration"
+)
+
+for json in "${jsons[@]}"; do
+  # ensure JSON is valid
+  echo "$json" | jq '.'
+done
+
+om-linux \
+  --target https://opsman.$ERT_DOMAIN \
+  --skip-ssl-validation \
+  --username $OPSMAN_USER \
+  --password $OPSMAN_PASSWORD \
+  configure-bosh \
+  --iaas-configuration "$iaas_configuration" \
+  --director-configuration "$director_configuration" \
+  --az-configuration "$az_configuration" \
+  --networks-configuration "$networks_configuration" \
+  --network-assignment "$network_assignment" \
+  --security-configuration "$security_configuration"
