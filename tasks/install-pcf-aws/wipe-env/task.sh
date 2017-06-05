560c2fd0ced95dc926916a662f3b6288852bd999
Kris Hicks <khicks@pivotal.io>
2017-06-05 09:45:38 -0700

    destroy-aws uses om-linux over aws client

--- tasks/install-pcf-aws/wipe-env/task.sh
+++ tasks/install-pcf-aws/wipe-env/task.sh
@@ -5,25 +5,18 @@ root=$(pwd)
 
 cd pcf-pipelines/tasks/install-pcf-aws/terraform
 
+echo "Deleting PCF installation..."
+om-linux \
+  --target "https://${OPSMAN_URI}" \
+  --skip-ssl-validation \
+  --username $OPSMAN_USERNAME \
+  --password $OPSMAN_PASSWORD \
+  delete-installation
+
 export AWS_ACCESS_KEY_ID=${TF_VAR_aws_access_key}
 export AWS_SECRET_ACCESS_KEY=${TF_VAR_aws_secret_key}
 export AWS_DEFAULT_REGION=${TF_VAR_aws_region}
-export VPC_ID=$(
-  terraform state show -state "${root}/terraform-state/terraform.tfstate" aws_vpc.PcfVpc | grep ^id | awk '{print $3}'
-)
 
-instances=$(
-  aws ec2 describe-instances --filters "Name=vpc-id,Values=$VPC_ID" --output=json |
-  jq --raw-output '.Reservations[].Instances[].InstanceId'
-)
-if [[ -n "$instances" ]]; then
-  # Purge all BOSH-managed VMs from the VPC
-  echo "instances: $instances will be deleted......"
-  aws ec2 terminate-instances --instance-ids $instances
-  aws ec2 wait instance-terminated --instance-ids $instances
-fi
-
-set +e
 terraform destroy \
   -force \
   -var "opsman_ami=dontcare" \
