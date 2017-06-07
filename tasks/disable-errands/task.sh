fe5511f6402c504478b201de44f4b6dfed7e93d4
Kris Hicks <khicks@pivotal.io>
2017-06-07 15:43:53 -0700

    Add disable-errands task
    This replaces disable-ert-errands; it now is parameterized to disable
    specific errands (or all, or none) for the configured product.

---
+++ tasks/disable-errands/task.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+errands=''
+
+if [[ "$ERRANDS_TO_DISABLE" == "all" ]]; then
+  errands=$(
+    om-linux \
+    --target "https://${OPSMAN_URI}" \
+    --skip-ssl-validation \
+    --username "$OPSMAN_USERNAME" \
+    --password "$OPSMAN_PASSWORD" \
+    errands \
+    --product-name "$PRODUCT_NAME" |
+    tail -n+4 | head -n-1 | grep "true" | cut -d'|' -f2 | tr -d ' '
+  )
+elif [[ "$ERRANDS_TO_DISABLE" != "" ]] && [[ "$ERRANDS_TO_DISABLE" != "none" ]]; then
+  errands=$(echo "$ERRANDS_TO_DISABLE" | tr ',' '\n')
+fi
+
+if [[ -z "$errands" ]]; then
+  echo Nothing to do.
+  exit 0
+fi
+
+while read errand; do
+  echo -n Disabling $errand...
+  om-linux \
+    --target "https://${OPSMAN_URI}" \
+    --skip-ssl-validation \
+    --username "$OPSMAN_USERNAME" \
+    --password "$OPSMAN_PASSWORD" \
+    set-errand-state \
+    --product-name "$PRODUCT_NAME" \
+    --errand-name $errand \
+    --post-deploy-state "disabled"
+  echo done
+done < <(echo "$errands")
