7a97acaa4dc2e0e550a935eb2db22340772f4ade
Kris Hicks <khicks@pivotal.io>
2017-06-14 09:28:31 -0700

    Use configure-bosh in install-pcf/gcp
    [#147203889]

--- install-pcf/gcp/tasks/config-director/task.sh
+++ install-pcf/gcp/tasks/config-director/task.sh
@@ -1,97 +1,167 @@
 #!/bin/bash
-set -e
 
-cat > opsman.json <<EOF
-[{
-  "director_configuration": {
-    "director_configuration[ntp_servers_string]": "0.pool.ntp.org",
-    "director_configuration[metrics_ip]": "",
-    "director_configuration[resurrector_enabled]": "1",
-    "director_configuration[post_deploy_enabled]": "0",
-    "director_configuration[bosh_recreate_on_next_deploy]": "0",
-    "director_configuration[retry_bosh_deploys]": "1",
-    "director_configuration[hm_pager_duty_options][enabled]": "0",
-    "director_configuration[hm_emailer_options][enabled]": "0",
-    "director_configuration[blobstore_type]": "local",
-    "director_configuration[database_type]": "internal",
-    "director_configuration[max_threads]": "",
-    "director_configuration[director_hostname]": ""
-  },
-  "availability_zones": {
-    "availability_zones": ["${gcp_zone_1}","${gcp_zone_2}","${gcp_zone_3}"],
-    "pipeline_extension": "fn_form_gen_availability_zones"
-  },
-  "networks": {
-    "infrastructure[icmp_checks_enabled]": "0",
-    "network_collection[networks_attributes][0][guid]": "0",
-    "network_collection[networks_attributes][0][name]": "infrastructure",
-    "network_collection[networks_attributes][0][service_network]": "0",
-    "network_collection[networks_attributes][0][subnets][0][iaas_identifier]": "${gcp_resource_prefix}-virt-net/${gcp_resource_prefix}-subnet-infrastructure-${gcp_region}/${gcp_region}",
-    "network_collection[networks_attributes][0][subnets][0][cidr]": "192.168.101.0/26",
-    "network_collection[networks_attributes][0][subnets][0][reserved_ip_ranges]": "192.168.101.1-192.168.101.9",
-    "network_collection[networks_attributes][0][subnets][0][dns]": "192.168.101.1,8.8.8.8",
-    "network_collection[networks_attributes][0][subnets][0][gateway]": "192.168.101.1",
-    "network_collection[networks_attributes][0][subnets][0][availability_zone_references][]": ["${gcp_zone_1}","${gcp_zone_2}","${gcp_zone_3}"],
-    "network_collection[networks_attributes][1][guid]": "1",
-    "network_collection[networks_attributes][1][name]": "ert",
-    "network_collection[networks_attributes][1][service_network]": "0",
-    "network_collection[networks_attributes][1][subnets][0][iaas_identifier]": "${gcp_resource_prefix}-virt-net/${gcp_resource_prefix}-subnet-ert-${gcp_region}/${gcp_region}",
-    "network_collection[networks_attributes][1][subnets][0][cidr]": "192.168.16.0/22",
-    "network_collection[networks_attributes][1][subnets][0][reserved_ip_ranges]": "192.168.16.1-192.168.16.9",
-    "network_collection[networks_attributes][1][subnets][0][dns]": "192.168.16.1,8.8.8.8",
-    "network_collection[networks_attributes][1][subnets][0][gateway]": "192.168.16.1",
-    "network_collection[networks_attributes][1][subnets][0][availability_zone_references][]": ["${gcp_zone_1}","${gcp_zone_2}","${gcp_zone_3}"],
-    "network_collection[networks_attributes][2][guid]": "2",
-    "network_collection[networks_attributes][2][name]": "services-1",
-    "network_collection[networks_attributes][2][service_network]": "1",
-    "network_collection[networks_attributes][2][subnets][0][iaas_identifier]": "${gcp_resource_prefix}-virt-net/${gcp_resource_prefix}-subnet-services-1-${gcp_region}/${gcp_region}",
-    "network_collection[networks_attributes][2][subnets][0][cidr]": "192.168.20.0/22",
-    "network_collection[networks_attributes][2][subnets][0][reserved_ip_ranges]": "192.168.20.1-192.168.20.9",
-    "network_collection[networks_attributes][2][subnets][0][dns]": "192.168.20.1,8.8.8.8",
-    "network_collection[networks_attributes][2][subnets][0][gateway]": "192.168.20.1",
-    "network_collection[networks_attributes][2][subnets][0][availability_zone_references][]": ["${gcp_zone_1}","${gcp_zone_2}","${gcp_zone_3}"],
-    "pipeline_extension": "fn_form_gen_networks"
-  },
-  "az_and_network_assignment": {
-    "bosh_product[singleton_availability_zone_reference]": "${gcp_zone_1}",
-    "bosh_product[network_reference]": "infrastructure",
-    "pipeline_extension": "fn_form_gen_az_and_network_assignment"
-  },
-  "resources":{
-    "product_resources_form[director][disk_type_id]": "",
-    "product_resources_form[director][vm_type_id]": "",
-    "product_resources_form[director][elb_names]": "",
-    "product_resources_form[director][internet_connected]": "0",
-    "product_resources_form[compilation][disk_type_id]": "",
-    "product_resources_form[compilation][vm_type_id]": "",
-    "product_resources_form[compilation][elb_names]": "",
-    "product_resources_form[compilation][internet_connected]": "0"
-  }
-}]
+set -eu
+
+iaas_configuration=$(
+  echo '{}' |
+  jq \
+    --arg gcp_project "$GCP_PROJECT_ID" \
+    --arg default_deployment_tag "$GCP_RESOURCE_PREFIX" \
+    --arg auth_json "$GCP_SERVICE_ACCOUNT_KEY" \
+    '. +
+    {
+      "project": $gcp_project,
+      "default_deployment_tag": $default_deployment_tag,
+      "auth_json": $auth_json
+    }
+    '
+)
+
+availability_zones="${GCP_ZONE_1},${GCP_ZONE_2},${GCP_ZONE_3}"
+
+az_configuration=$(
+  echo '{}' |
+  jq \
+    --arg availability_zones "$availability_zones" \
+    '. +
+    {
+      "availability_zones": ($availability_zones | split(",") | map({name: .}))
+    }'
+)
+
+network_configuration=$(
+  echo '{}' |
+  jq \
+    --argjson icmp_checks_enabled false \
+    --arg infra_network_name "infrastructure" \
+    --arg infra_vcenter_network "${GCP_RESOURCE_PREFIX}-virt-net/${GCP_RESOURCE_PREFIX}-subnet-infrastructure-${GCP_REGION}/${GCP_REGION}" \
+    --arg infra_network_cidr "192.168.101.0/26" \
+    --arg infra_reserved_ip_ranges "192.168.101.1-192.168.101.9" \
+    --arg infra_dns "192.168.101.1,8.8.8.8" \
+    --arg infra_gateway "192.168.101.1" \
+    --arg infra_availability_zones "$availability_zones" \
+    --arg deployment_network_name "ert" \
+    --arg deployment_vcenter_network "${GCP_RESOURCE_PREFIX}-virt-net/${GCP_RESOURCE_PREFIX}-subnet-ert-${GCP_REGION}/${GCP_REGION}" \
+    --arg deployment_network_cidr "192.168.16.0/22" \
+    --arg deployment_reserved_ip_ranges "192.168.16.1-192.168.16.9" \
+    --arg deployment_dns "192.168.16.1,8.8.8.8" \
+    --arg deployment_gateway "192.168.16.1" \
+    --arg deployment_availability_zones "$availability_zones" \
+    --argjson services_network_is_service_network true \
+    --arg services_network_name "services-1" \
+    --arg services_vcenter_network "${GCP_RESOURCE_PREFIX}-virt-net/${GCP_RESOURCE_PREFIX}-subnet-services-1-${GCP_REGION}/${GCP_REGION}" \
+    --arg services_network_cidr "192.168.20.0/22" \
+    --arg services_reserved_ip_ranges "192.168.20.1-192.168.20.9" \
+    --arg services_dns "192.168.20.1,8.8.8.8" \
+    --arg services_gateway "192.168.20.1" \
+    --arg services_availability_zones "$availability_zones" \
+    '. +
+    {
+      "icmp_checks_enabled": $icmp_checks_enabled,
+      "networks": [
+        {
+          "name": $infra_network_name,
+          "service_network": false,
+          "subnets": [
+            {
+              "iaas_identifier": $infra_vcenter_network,
+              "cidr": $infra_network_cidr,
+              "reserved_ip_ranges": $infra_reserved_ip_ranges,
+              "dns": $infra_dns,
+              "gateway": $infra_gateway,
+              "availability_zones": ($infra_availability_zones | split(","))
+            }
+          ]
+        },
+        {
+          "name": $deployment_network_name,
+          "service_network": false,
+          "subnets": [
+            {
+              "iaas_identifier": $deployment_vcenter_network,
+              "cidr": $deployment_network_cidr,
+              "reserved_ip_ranges": $deployment_reserved_ip_ranges,
+              "dns": $deployment_dns,
+              "gateway": $deployment_gateway,
+              "availability_zones": ($deployment_availability_zones | split(","))
+            }
+          ]
+        },
+        {
+          "name": $services_network_name,
+          "service_network": $services_network_is_service_network,
+          "subnets": [
+            {
+              "iaas_identifier": $services_vcenter_network,
+              "cidr": $services_network_cidr,
+              "reserved_ip_ranges": $services_reserved_ip_ranges,
+              "dns": $services_dns,
+              "gateway": $services_gateway,
+              "availability_zones": ($services_availability_zones | split(","))
+            }
+          ]
+        }
+      ]
+    }'
+)
+
+director_config=$(cat <<-EOF
+{
+  "ntp_servers_string": "0.pool.ntp.org",
+  "resurrector_enabled": true,
+  "retry_bosh_deploys": true,
+  "database_type": "internal",
+  "blobstore_type": "local"
+}
 EOF
+)
 
-iaas_configuration_json=$(echo "{
-  \"iaas_configuration[project]\": \"${gcp_proj_id}\",
-  \"iaas_configuration[default_deployment_tag]\": \"${gcp_resource_prefix}\",
-  \"access_type\": \"keys\",
-  \"iaas_configuration[auth_json]\":
-    $(echo ${gcp_svc_acct_key})
-}")
+resource_configuration=$(cat <<-EOF
+{
+  "director": {
+    "internet_connected": false
+  },
+  "compilation": {
+    "internet_connected": false
+  }
+}
+EOF
+)
 
-json_file_path="."
-opsman_host="opsman.${pcf_ert_domain}"
+security_configuration=$(
+  echo '{}' |
+  jq \
+    --arg trusted_certificates "" \
+    '. +
+    {
+      "trusted_certificates": $trusted_certificates,
+      "vm_password_type": "generate"
+    }'
+)
 
-# -
-source pcf-pipelines/install-pcf/gcp/functions/misc.sh
-# opsman_host
-source pcf-pipelines/install-pcf/gcp/functions/config-director-json-fn-opsman-curl.sh
-# json_file_path
-source pcf-pipelines/install-pcf/gcp/functions/config-director-json-fn-opsman-auth.sh
-# json_file_path
-source pcf-pipelines/install-pcf/gcp/functions/config-director-json-fn-opsman-json-to-post-data.sh
-# opsman_host, pcf_opsman_admin_passwd
-source pcf-pipelines/install-pcf/gcp/functions/config-director-json-fn-opsman-extensions.sh
-# -
-source pcf-pipelines/install-pcf/gcp/functions/config-director-json-fn-opsman-config-director.sh
+network_assignment=$(
+  echo '{}' |
+  jq \
+    --arg availability_zones "$availability_zones" \
+    --arg network "infrastructure" \
+    '. +
+    {
+      "singleton_availability_zone": ($availability_zones | split(",") | .[0]),
+      "network": $network
+    }'
+)
 
-fn_config_director
+echo "Configuring IaaS and Director..."
+om-linux \
+  --target https://$OPS_MGR_HOST \
+  --skip-ssl-validation \
+  --username $OPS_MGR_USR \
+  --password $OPS_MGR_PWD \
+  configure-bosh \
+  --iaas-configuration "$iaas_configuration" \
+  --director-configuration "$director_config" \
+  --az-configuration "$az_configuration" \
+  --networks-configuration "$network_configuration" \
+  --network-assignment "$network_assignment" \
+  --security-configuration "$security_configuration" \
+  --resource-configuration "$resource_configuration"
