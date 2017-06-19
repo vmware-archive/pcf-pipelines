0c9787dcdc4c2c57a16f73a9fa71ac1b1ec2794d
John Calabrese <jcalabrese@pivotal.io>
2017-06-19 14:25:12 -0400

    add cidr allow definition to wipe-env for aws. 'dontcare' or 'default' doesnt work
    [#143475959]

--- install-pcf/aws/tasks/wipe-env/task.sh
+++ install-pcf/aws/tasks/wipe-env/task.sh
@@ -21,12 +21,29 @@ export AWS_ACCESS_KEY_ID=${TF_VAR_aws_access_key}
 export AWS_SECRET_ACCESS_KEY=${TF_VAR_aws_secret_key}
 export AWS_DEFAULT_REGION=${TF_VAR_aws_region}
 
+OPSMAN_ALLOW_SSH=0
+OPSMAN_ALLOW_SSH_CIDR_LIST='["0.0.0.0/32"]'
+if [[ -n "${OPSMAN_ALLOW_SSH_CIDR_RANGES// }" ]]; then
+  OPSMAN_ALLOW_SSH=1
+  OPSMAN_ALLOW_SSH_CIDR_LIST='["'${OPSMAN_ALLOW_SSH_CIDR_RANGES/\,/\"\,\"}'"]'
+fi
+
+OPSMAN_ALLOW_HTTPS=0
+OPSMAN_ALLOW_HTTPS_CIDR_LIST='["0.0.0.0/32"]'
+if [[ -n "${OPSMAN_ALLOW_HTTPS_CIDR_RANGES// }" ]]; then
+  OPSMAN_ALLOW_HTTPS=1
+  OPSMAN_ALLOW_HTTPS_CIDR_LIST='["'${OPSMAN_ALLOW_HTTPS_CIDR_RANGES/\,/\"\,\"}'"]'
+fi
+
 terraform destroy \
   -force \
   -var "opsman_ami=dontcare" \
   -var "db_master_username=dontcare" \
   -var "db_master_password=dontcare" \
   -var "prefix=dontcare" \
-  -var "opsman_allow_cidr=dontcare" \
+  -var "opsman_allow_ssh=${OPSMAN_ALLOW_SSH}" \
+  -var "opsman_allow_ssh_cidr_ranges=${OPSMAN_ALLOW_SSH_CIDR_LIST}" \
+  -var "opsman_allow_https=${OPSMAN_ALLOW_HTTPS}" \
+  -var "opsman_allow_https_cidr_ranges=${OPSMAN_ALLOW_HTTPS_CIDR_LIST}" \
   -state "${root}/terraform-state/terraform.tfstate" \
   -state-out "${root}/terraform-state-output/terraform.tfstate"
