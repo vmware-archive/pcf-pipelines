9a5ef7765c19c64b6e422d9c4ee66fb978202c7f
Kris Hicks <khicks@pivotal.io>
2017-06-19 17:27:20 -0700

    Use 3600 second upload-product request timeout
    ERT at 7GB occasionally runs into the default 30 minute timeout.

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -46,4 +46,4 @@ if [ -n "$STEMCELL_VERSION" ]; then
 fi
 
 FILE_PATH=`find ./pivnet-product -name *.pivotal`
-om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k upload-product -p $FILE_PATH
+om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 3600 upload-product -p $FILE_PATH
