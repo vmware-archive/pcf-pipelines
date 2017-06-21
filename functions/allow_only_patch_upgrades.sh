dcb36791ebc7472175515415fcce67b1bdcd0be2
John Calabrese <jcalabrese@pivotal.io>
2017-06-21 16:18:26 -0400

    implement version check gate at begining of pipelines to fail on non-patch upgrade attempts
    [#146902091]

--- functions/allow_only_patch_upgrades.sh
+++ functions/allow_only_patch_upgrades.sh
@@ -2,43 +2,41 @@
 
 set -eu
 # 
-# Gets all major/minor versions for deployed products
-# and compares them to all major/minor versions
-# that are currently staged in ops manager
+# Gets major/minor version for deployed product
+# and compares them to the product version you are
+# trying to install
 # if there is any difference (not including patch)
 # then we should fail
 #
 function allow_only_patch_upgrades {
-  if [ "$#" -ne 3 ]; then
+  if [ "$#" -ne 5 ]; then
       echo "Illegal number of arguments."
-      echo "usage: allow_only_patch_upgrades <opsman_uri> <opsman_user> <opsman_pass>"
+      echo "usage: allow_only_patch_upgrades <opsman_uri> <opsman_user> <opsman_pass> <product_name> <product_resource_dir>"
   fi
   local OPS_MGR_HOST=$1
   local OPS_MGR_USR=$2
   local OPS_MGR_PWD=$3
-  local staged_versions=$(
+  local PRODUCT_NAME=$4
+  local PRODUCT_DIR=$5
+
+  local deployed_version=$(
     om-linux \
       --target "https://${OPS_MGR_HOST}" \
       --username "${OPS_MGR_USR}" \
       --password "${OPS_MGR_PWD}" \
       --skip-ssl-validation \
-      staged-products | awk -F"|" '{print $3 }' | awk -F"." '{print $1"."$2}' | egrep -v "VERSION|^\." | uniq | sort
+      deployed-products | grep "${PRODUCT_NAME}" | awk -F"|" '{print $3 }' | awk -F"." '{print $1"."$2}'
     )
+  if [[ `ls ${PRODUCT_DIR} | grep "${deployed_version}"` ]]; then
+    echo "we have a safe upgrade for version: ${deployed_version}";
 
-  local deployed_versions=$(
-    om-linux \
-      --target "https://${OPS_MGR_HOST}" \
-      --username "${OPS_MGR_USR}" \
-      --password "${OPS_MGR_PWD}" \
-      --skip-ssl-validation \
-      deployed-products | awk -F"|" '{print $3 }' | awk -F"." '{print $1"."$2}' | egrep -v "VERSION|^\." | uniq | sort
-    )
-
-  if [[ $staged_versions != $deployed_versions ]]; then 
-    echo "Staged versions: "
-    echo "$staged_versions"
-    echo "Deployed versions: "
-    echo "$deployed_versions"
+  else
+    echo "You are trying to install version: "
+    ls ${PRODUCT_DIR}
+    echo
+    echo "Your currently deployed version is: "
+    echo "$deployed_version"
+    echo
     echo "Pivotal recommends that you only automate
     patch upgrades of PCF, and perform major or minor
     upgrades manually to ensure that no high-impact
