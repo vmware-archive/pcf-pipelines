718f3e17d24f71677f141e0b3028ff7a503c903f
Beibei Yang <beibei.yang@emc.com>
2017-06-27 10:50:31 -0400

    add blobstore_internal_access_rules to support Whitelist non-RFC-1918 Private Networks in vsphere and GCP

--- tasks/config-ert/load_cf_properties.sh
+++ tasks/config-ert/load_cf_properties.sh
@@ -64,6 +64,7 @@ jq \
   --arg mysql_backups_scp_key "$MYSQL_BACKUPS_SCP_KEY" \
   --arg mysql_backups_scp_destination "$MYSQL_BACKUPS_SCP_DESTINATION" \
   --arg mysql_backups_scp_cron_schedule "$MYSQL_BACKUPS_SCP_CRON_SCHEDULE" \
+  --arg blobstore_internal_access_rules "$BLOBSTORE_INTERNAL_ACCESS_RULES" \
   '
   . +
   {
@@ -358,6 +359,21 @@ jq \
   else
     .
   end
+
+  +
+
+  # Advanced Features
+  if $blobstore_internal_access_rules != "" then
+    {
+      ".nfs_server.blobstore_internal_access_rules": {
+      "value": $blobstore_internal_access_rules
+      }
+    }
+  else
+    .
+  end
+    
+
   ' > cf_properties
 
 cf_properties=$(cat cf_properties)
