0baa6e4fbb85d86a133122920e7dcb97f3d9b7a4
Kris Hicks <khicks@pivotal.io>
2017-06-27 08:10:48 -0700

    Refactor update_pcf_pipelines script

--- ci/scripts/update_pcf_pipelines.sh
+++ ci/scripts/update_pcf_pipelines.sh
@@ -10,27 +10,34 @@ function cleanup {
 
 trap cleanup EXIT
 
-install_pcf_aws_current_params=$(lpass show Shared-Customer\ [0]/lre1-aws/install-pcf-params --notes)
-upgrade_ert_aws_current_params=$(lpass show Shared-Customer\ [0]/lre1-aws/ert-upgrade-params --notes)
-
 tmpfile=$(mktemp)
 
 IFS=$'\n'
 
-cat > $tmpfile <<EOF
-install_pcf_aws_current_params: |
+function write_params() {
+  local file=$1
+  local note_path=$2
+  local key=$3
+
+  local lpass_path="Shared-Customer [0]/${note_path}"
+
+  local contents=$(lpass show $lpass_path  --notes)
+
+  if [[ -z "$contents" ]]; then
+    echo Could not fetch contents from $lpass_path
+    exit 1
+  fi
+
+  cat >> $file <<EOF
+$key: |
 EOF
 
-for line in $install_pcf_aws_current_params; do
-  echo "  $line" >> $tmpfile
-done
+  for line in $contents; do
+    echo "  $line" >> $file
+  done
+}
 
-cat >> $tmpfile <<EOF
-upgrade_ert_aws_current_params: |
-EOF
-
-for line in $upgrade_ert_aws_current_params; do
-  echo "  $line" >> $tmpfile
-done
+write_params $tmpfile "lre1-aws/install-pcf-params" "install_pcf_aws_current_params"
+write_params $tmpfile "lre1-aws/ert-upgrade-params" "upgrade_ert_aws_current_params"
 
 fly -tc02 sp -p pcf-pipelines -c ci/pcf-pipelines/pipeline.yml -l $tmpfile -l <(lpass show pcf-pipelines-params --notes)
