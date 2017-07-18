672151513367a21d5549c34dc839f49df12036aa
John Calabrese <jcalabrese@pivotal.io>
2017-07-18 12:11:49 -0400

    store darknet tarballs to s3 directory
    [#149036597]

--- download-offline-resources/scripts/unpack.sh
+++ download-offline-resources/scripts/unpack.sh
@@ -12,9 +12,22 @@ function unpack () {
       '{system("echo unpacking: "$1"; mkdir -p "dest_dir"/"$1"; tar -xvzf "target_dir"/"$1".tgz -C "dest_dir"/"$1)}'
 }
 
-if [[ $# != 2 ]]; then
-  echo "usage: $0 <target> <dest>"
+function store () {
+  local endpoint=$1
+  local source_dir=$2
+  local bucket_name=$3
+  local remote_folder_name=$4
+  aws --endpoint-url=${endpoint} s3 cp ${source_dir} s3://${bucket_name}/${remote_folder_name} --recursive --debug
+}
+
+if [[ $# != 4 ]]; then
+  echo "usage: $0 <tarball-dir-target> <unpack-dest> <s3-endpoint> <bucket-name>"
   exit 1
 fi
-
-unpack $1 $2
+tarball_target=$1
+unpack_dest=$2
+s3_endpoint=$3
+bucket_name=$4
+remote_folder_name=$(basename ${unpack_dest})
+unpack ${tarball_target} ${unpack_dest}
+store ${s3_endpoint} ${unpack_dest} ${bucket_name} ${remote_folder_name}
