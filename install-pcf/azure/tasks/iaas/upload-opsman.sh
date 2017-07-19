cb8a587dd433d575dc1b392c3f07b2d17f7be60d
Gavin Enns <genns@pivotal.io>
2017-07-19 10:43:10 -0400

    parse ops manager yml from pivnet for azure vhd url
    [#148603651]
    
    Signed-off-by: John Calabrese <xchapter7x@gmail.com>

--- install-pcf/azure/tasks/iaas/upload-opsman.sh
+++ install-pcf/azure/tasks/iaas/upload-opsman.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-set -e
+set -ue
 
-#############################################################
-#################### Azure Auth  & functions ##################
-#############################################################
-
-
-### Grab the OpsManager image name from the PivNet PDF
+current_dir=$( dirname "${BASH_SOURCE[0]}" )
+source "${current_dir}/parse_opsman_region_url.sh"
 echo "=============================================================================================="
-echo "Getting Azure Ops Manager VHD URI ...."
+echo "Getting Azure Ops Manager VHD URI from Pivnet YML...."
 echo "=============================================================================================="
-
-/usr/bin/pdftotext pivnet-opsmgr/*Azure*.pdf /tmp/opsman.txt
-opsman_region="East US"
-pcf_opsman_image_vhd=$(grep -i -A 1 "$opsman_region" /tmp/opsman.txt | grep "https")
-#pcf_opsman_image_version=$(grep -i -A 1 "$opsman_region" /tmp/opsman.txt | grep "https" | awk -F "ops-manager-" '{print$2}')
-
-
+pcf_opsman_image_vhd=$(parseRegionURL "east_us" "pivnet-opsmgr/*Azure.yml")
 echo "Found Azure OpsMan Image @ $pcf_opsman_image_vhd ...."
 echo $pcf_opsman_image_vhd > opsman-metadata/uri
