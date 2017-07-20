10c4bb6189536a2b683b526254c0dd909aa42883
Kris Hicks <khicks@pivotal.io>
2017-07-20 08:41:24 -0700

    Write nested params in update_pcf_pipelines script

--- ci/scripts/update_pcf_pipelines.sh
+++ ci/scripts/update_pcf_pipelines.sh
@@ -16,11 +16,10 @@ IFS=$'\n'
 
 function write_params() {
   local file=$1
-  local note_path=$2
+  local prefix=$2
   local key=$3
-
+  local note_path=$4
   local lpass_path="Shared-Customer [0]/${note_path}"
-
   local contents=$(lpass show $lpass_path  --notes)
 
   if [[ -z "$contents" ]]; then
@@ -29,24 +28,27 @@ function write_params() {
   fi
 
   cat >> $file <<EOF
-$key: |
+$prefix$key: |
 EOF
 
   for line in $contents; do
-    echo "  $line" >> $file
+    echo "  ${prefix}${line}" >> $file
   done
 }
 
-write_params $tmpfile "lre1-aws/install-pcf-params" "install_pcf_aws_current_params"
-write_params $tmpfile "lre1-aws/ert-upgrade-params" "upgrade_ert_aws_current_params"
-write_params $tmpfile "lre1-gcp/install-pcf-params" "install_pcf_gcp_current_params"
-write_params $tmpfile "lre1-gcp/ert-upgrade-params" "upgrade_ert_gcp_current_params"
-write_params $tmpfile "vsphere-slot1/install-pcf-params" "install_pcf_vsphere_slot1_params"
-write_params $tmpfile "vsphere-slot1/upgrade-ert-params" "upgrade_ert_vsphere_slot1_params"
-write_params $tmpfile "vsphere-slot1/upgrade-ops-manager-params" "upgrade_ops_manager_vsphere_slot1_params"
-write_params $tmpfile "vsphere-slot6/install-pcf-params" "install_pcf_vsphere_darknet_params"
-write_params $tmpfile "create-offline-pinned-pipelines-params" "create_offline_pinned_pipelines_params"
-write_params $tmpfile "unpack-pcf-pipelines-combined-params" "unpack_pcf_pipelines_combined_params"
+write_params $tmpfile "" "install_pcf_aws_current_params" 'lre1-aws/install-pcf-params'
+write_params $tmpfile "" "upgrade_ert_aws_current_params" 'lre1-aws/ert-upgrade-params'
+write_params $tmpfile "" "install_pcf_gcp_current_params" 'lre1-gcp/install-pcf-params'
+write_params $tmpfile "" "upgrade_ert_gcp_current_params" 'lre1-gcp/ert-upgrade-params'
+write_params $tmpfile "" "install_pcf_vsphere_slot1_params" 'vsphere-slot1/install-pcf-params'
+write_params $tmpfile "" "upgrade_ert_vsphere_slot1_params" 'vsphere-slot1/upgrade-ert-params'
+write_params $tmpfile "" "upgrade_ops_manager_vsphere_slot1_params" 'vsphere-slot1/upgrade-ops-manager-params'
+write_params $tmpfile "" "create_offline_pinned_pipelines_params" 'create-offline-pinned-pipelines-params'
+write_params $tmpfile "" "unpack_pcf_pipelines_combined_params" 'unpack-pcf-pipelines-combined-params'
+write_params $tmpfile "  " "install_pcf_pipeline_params" 'vsphere-slot6/install-pcf-params'
+cat >> $tmpfile <<EOF
+  install_pcf_pipeline_name: install-pcf-vsphere-darknet-current
+EOF
 
 fly -tlrp3 sp -p pcf-pipelines -c ci/pcf-pipelines/pipeline.yml \
   -l $tmpfile \
