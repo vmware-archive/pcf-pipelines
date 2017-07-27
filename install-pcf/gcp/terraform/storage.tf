5d71b41346603726ad3c50f3eea8683920fbbcdd
Alex Guedes <alex.guedes@gmail.com>
2017-07-27 14:19:13 +0100

    Added location to the Storage Bucket on GCP

--- install-pcf/gcp/terraform/storage.tf
+++ install-pcf/gcp/terraform/storage.tf
@@ -1,19 +1,23 @@
 resource "google_storage_bucket" "buildpacks" {
   name          = "${var.prefix}-buildpacks"
+  location      = "${var.gcp_storage_bucket_location}"
   force_destroy = true
 }
 
 resource "google_storage_bucket" "droplets" {
   name          = "${var.prefix}-droplets"
+  location      = "${var.gcp_storage_bucket_location}"
   force_destroy = true
 }
 
 resource "google_storage_bucket" "packages" {
   name          = "${var.prefix}-packages"
+  location      = "${var.gcp_storage_bucket_location}"
   force_destroy = true
 }
 
 resource "google_storage_bucket" "resources" {
   name          = "${var.prefix}-resources"
+  location      = "${var.gcp_storage_bucket_location}"
   force_destroy = true
 }
