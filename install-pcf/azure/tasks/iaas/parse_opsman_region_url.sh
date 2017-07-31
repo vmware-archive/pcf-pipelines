93e72e24b29fe92894f7283dfc43134c0b3cbdb2
Christian Ang <cang@pivotal.io>
2017-07-31 16:45:50 -0700

    Remove quote around yaml_file_path
    - in parse_opsman_region_url
    - subtle bug using bash. Quoting the path doesn't allow the * to expand into
    the correct path.
    
    [#148603651]

--- install-pcf/azure/tasks/iaas/parse_opsman_region_url.sh
+++ install-pcf/azure/tasks/iaas/parse_opsman_region_url.sh
@@ -3,6 +3,6 @@
 function parseRegionURL () {
      region_name=$1
      yaml_file_path=$2
-     pcf_opsman_image_vhd=$(grep -i "${region_name}:.*.vhd" "${yaml_file_path}" | cut -d' ' -f2)
+     pcf_opsman_image_vhd=$(grep -i "${region_name}:.*.vhd" ${yaml_file_path} | cut -d' ' -f2)
      echo "${pcf_opsman_image_vhd}"
 }
