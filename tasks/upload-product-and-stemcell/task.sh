d23e8c0eba1e3d032db204a78efccbe29e2efa33
Joe George <joegeorge01@gmail.com>
2017-08-02 15:06:43 -0400

    Add support for windows stemcells
    Set the product plug for the stemcell based on whether or not, a windows stemcell is required.

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -18,6 +18,23 @@ STEMCELL_VERSION=$(
     '
 )
 
+set +e
+cat ./pivnet-product/metadata.json | jq -e \
+    '
+    [
+      .Dependencies[]
+      | select(.Release.Product.Name | contains("Stemcells for PCF (Windows)"))
+      | .Release.Version
+    ] | sort | last // empty
+    ' > /dev/null
+
+if [ $? -eq 0 ]; then
+  STEMCELL_PLUG=stemcells-windows-server
+else
+  STEMCELL_PLUG=stemcells
+fi
+set -e
+
 if [ -n "$STEMCELL_VERSION" ]; then
   diagnostic_report=$(
     om-linux \
@@ -39,7 +56,7 @@ if [ -n "$STEMCELL_VERSION" ]; then
   if [[ -z "$stemcell" ]]; then
     echo "Downloading stemcell $STEMCELL_VERSION"
     pivnet-cli login --api-token="$PIVNET_API_TOKEN"
-    pivnet-cli download-product-files -p stemcells -r $STEMCELL_VERSION -g "*${IAAS}*" --accept-eula
+    pivnet-cli download-product-files -p $STEMCELL_PLUG -r $STEMCELL_VERSION -g "*${IAAS}*" --accept-eula
 
     SC_FILE_PATH=`find ./ -name *.tgz`
 
