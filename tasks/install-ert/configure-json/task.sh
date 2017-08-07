2cf77adfb96719a62b4983f5e1239228908f3fc2
Kris Hicks <khicks@pivotal.io>
2017-08-07 16:36:06 -0700

    Inline iaas-specific configure-json scripts

--- tasks/install-ert/configure-json/task.sh
+++ tasks/install-ert/configure-json/task.sh
@@ -155,7 +155,36 @@ do
    sed -i -e "s/$templateplaceholder/${varvalue}/g" ${json_file}
 done
 
-if [[ -e pcf-pipelines/tasks/install-ert/scripts/iaas-specific-config/${pcf_iaas}/run.sh ]]; then
-  echo "Executing ${pcf_iaas} IaaS specific config ..."
-  ./pcf-pipelines/tasks/install-ert/scripts/iaas-specific-config/${pcf_iaas}/run.sh
+if [[ "${pcf_iaas}" == "aws" ]]; then
+  cd terraform-state
+    output_json=$(terraform output --json -state *.tfstate)
+    db_host=$(echo $output_json | jq --raw-output '.db_host.value')
+    aws_region=$(echo $output_json | jq --raw-output '.region.value')
+    aws_access_key=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^id | awk '{print $3}'`
+    aws_secret_key=`terraform state show aws_iam_access_key.pcf_iam_user_access_key | grep ^secret | awk '{print $3}'`
+  cd -
+
+  sed -i \
+    -e "s%{{db_host}}%${db_host}%g" \
+    -e "s%{{aws_access_key}}%${aws_access_key}%g" \
+    -e "s%{{aws_secret_key}}%${aws_secret_key}%g" \
+    -e "s%{{aws_region}}%${aws_region}%g" \
+    -e "s%{{s3_endpoint}}%${S3_ENDPOINT}%g" \
+    -e "s%{{syslog_host}}%${SYSLOG_HOST}%g" \
+    ${json_file}
+elif [[ "${pcf_iaas}" == "gcp" ]]; then
+  cd terraform-state
+    db_host=$(terraform output --json -state *.tfstate | jq --raw-output '.sql_instance_ip.value')
+  cd -
+
+  if [ -z "$db_host" ]; then
+    echo Failed to get SQL instance IP from Terraform state file
+    exit 1
+  fi
+
+  sed -i \
+    -e "s%{{db_host}}%${db_host}%g" \
+    -e "s%{{gcp_storage_access_key}}%${gcp_storage_access_key}%g" \
+    -e "s%{{gcp_storage_secret_key}}%${gcp_storage_secret_key}%g" \
+    ${json_file}
 fi
