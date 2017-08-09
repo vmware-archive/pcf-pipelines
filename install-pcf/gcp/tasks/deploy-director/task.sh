2b094bec9baef66a1ed50c9a564b6e842e3209b2
Kris Hicks <khicks@pivotal.io>
2017-08-09 08:35:39 -0700

    Remove gcp/tasks/deploy-director
    Use normal apply-changes task instead.

--- install-pcf/gcp/tasks/deploy-director/task.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-om-linux --target "https://opsman.${pcf_ert_domain}" -k \
-  --username "$pcf_opsman_admin_username" \
-  --password "$pcf_opsman_admin_password" \
-  apply-changes
