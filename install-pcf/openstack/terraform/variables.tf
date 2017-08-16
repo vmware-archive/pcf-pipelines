d6f915b12878786ed059dbad745187ba2953c18b
Slawek Ligus <sligus@pivotal.io>
2017-08-16 10:37:49 -0700

    Adding provider and network TF templates.
    [#149863258]
    
    Signed-off-by: Christian Ang <cang@pivotal.io>

---
+++ install-pcf/openstack/terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "os_tenant_name" {}
+variable "os_username" {}
+variable "os_password" {}
+variable "os_auth_url" {}
+variable "os_region" {}
+variable "os_domain_name" {}
+
+variable "prefix" {}
+
+variable "infra_subnet_cidr" {}
+variable "ert_subnet_cidr" {}
+variable "services_subnet_cidr" {}
+variable "dynamic_services_subnet_cidr" {}
+
+variable "infra_dns" {}
+variable "ert_dns" {}
+variable "services_dns" {}
+variable "dynamic_services_dns" {}
+
+variable "external_network_id" {}
+
