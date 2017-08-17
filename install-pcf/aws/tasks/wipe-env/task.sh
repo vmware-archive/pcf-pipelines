de010643100383acbda6ae41f8f02f6c11629099
Darshan Kothari <dkothari@corelogic.com>
2017-08-16 18:55:14 -0700

    need to have backslashes instead of forward slashes

--- install-pcf/aws/tasks/wipe-env/task.sh
+++ install-pcf/aws/tasks/wipe-env/task.sh
@@ -19,19 +19,19 @@ fi
 
 terraform destroy \
   -force \
-  -var "aws_access_key=${AWS_ACCESS_KEY_ID}" /
-  -var "aws_secret_key=${AWS_SECRET_ACCESS_KEY}" /
-  -var "aws_key_name=dontcare" /
-  -var "aws_cert_arn=dontcare" /
+  -var "aws_access_key=${AWS_ACCESS_KEY_ID}" \
+  -var "aws_secret_key=${AWS_SECRET_ACCESS_KEY}" \
+  -var "aws_key_name=dontcare" \
+  -var "aws_cert_arn=dontcare" \
   -var "db_master_username=dontcare" \
   -var "db_master_password=dontcare" \
   -var "prefix=dontcare" \
   -var "opsman_ami=dontcare" \
-  -var "amis_nat=dontcare" /
-  -var "aws_region=dontcare" /
-  -var "az1=dontcare" /
-  -var "az2=dontcare" /
-  -var "az3=dontcare" /
-  -var "route53_zone_id=dontcare" /
+  -var "amis_nat=dontcare" \
+  -var "aws_region=dontcare" \
+  -var "az1=dontcare" \
+  -var "az2=dontcare" \
+  -var "az3=dontcare" \
+  -var "route53_zone_id=dontcare" \
   -state "${root}/terraform-state/terraform.tfstate" \
   -state-out "${root}/terraform-state-output/terraform.tfstate"
