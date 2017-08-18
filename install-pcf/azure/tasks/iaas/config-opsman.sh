ce86f0edbebd599d15a8073966c92a8ae48c1048
Christian Ang <cang@pivotal.io>
2017-08-18 11:00:59 -0700

    Uppercase config-opsman params
    [#150189672] Change opsman_uri parameter in our Azure pipeline to the new param name
    
    Signed-off-by: Slawek Ligus <sligus@pivotal.io>

--- install-pcf/azure/tasks/iaas/config-opsman.sh
+++ install-pcf/azure/tasks/iaas/config-opsman.sh
@@ -5,13 +5,12 @@ sudo cp tool-om/om-linux /usr/local/bin
 sudo chmod 755 /usr/local/bin/om-linux
 
 echo "=============================================================================================="
-echo "Configuring OpsManager @ https://opsman.$pcf_ert_domain ..."
+echo "Configuring OpsManager @ https://opsman.${PCF_ERT_DOMAIN} ..."
 echo "=============================================================================================="
 
-
 #Configure Opsman
-om-linux --target https://opsman.$pcf_ert_domain -k \
-     configure-authentication \
-       --username "$pcf_opsman_admin" \
-       --password "$pcf_opsman_admin_passwd" \
-       --decryption-passphrase "$pcf_opsman_admin_passwd"
+om-linux --target https://opsman.${PCF_ERT_DOMAIN} -k \
+  configure-authentication \
+  --username "${PCF_OPSMAN_ADMIN}" \
+  --password "${PCF_OPSMAN_ADMIN_PASSWORD}" \
+  --decryption-passphrase "${PCF_OPSMAN_ADMIN_PASSWORD}"
