9a5e608e70c7c18295f8e20836cf4e02c7658ace
Chris Mattingly <chris.mattingly@emc.com>
2017-10-24 11:08:38 -0400

    Update task.sh
    Fix error if folder already exists

--- install-pcf/vsphere/tasks/import-opsman/task.sh
+++ install-pcf/vsphere/tasks/import-opsman/task.sh
@@ -43,5 +43,8 @@ cat options.json
 if [ -z $OM_VM_FOLDER ]; then
   govc import.ova -options=options.json $file_path
 else
+  if [ `govc folder.info $OM_VM_FOLDER 2>&1 | grep $OM_VM_FOLDER | awk '{print $2}'` != $OM_VM_FOLDER ]; then
+    govc folder.create $OM_VM_FOLDER
+  fi
   govc import.ova -folder=$OM_VM_FOLDER -options=options.json $file_path
 fi
