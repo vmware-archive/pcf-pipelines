3043aefd4a7da258c8249594d51aeb08938401ec
Christian Ang <cang@pivotal.io>
2017-11-09 10:36:18 -0800

    Add ops file to trigger set pipeline on unpack
    [#152612692]

---
+++ ci/tasks/apply-patch/task.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+function main() {
+  local temp_pipeline_file=$(mktemp)
+  local yaml_patch_args=$(
+    echo "${PATCH_FILES}" | \
+      xargs -n 1 echo "-o")
+
+  cat "${PIPELINE_FILE}" | \
+    yaml_patch_linux ${yaml_patch_args} \
+    > "${temp_pipeline_file}"
+
+  cat "${temp_pipeline_file}" \
+    > "${PIPELINE_FILE}"
+}
+
+main
