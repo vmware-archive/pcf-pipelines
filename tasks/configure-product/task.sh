056e670f8eb9b432c2d43931022c6a541b9d0aff
Christian Ang <cang@pivotal.io>
2017-11-30 14:32:43 -0800

    Add configure-product task
    [#153262778]
    
    Signed-off-by: Luciano Silva <lsilva@pivotal.io>

---
+++ tasks/configure-product/task.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eu
+
+function main() {
+  local configuration_properties="$(cat ${PRODUCT_PROPERTIES_FILE})"
+  local configuration_network="$(cat ${PRODUCT_NETWORK_FILE})"
+  local configuration_resources="$(cat ${PRODUCT_RESOURCES_FILE})"
+
+  om-linux \
+    --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" \
+    --username "${OPS_MGR_USR}" \
+    --password "${OPS_MGR_PWD}" \
+    --skip-ssl-validation \
+    configure-product \
+    --product-name "${PRODUCT_NAME}" \
+    --product-properties "${configuration_properties}" \
+    --product-network "${configuration_network}" \
+    --product-resources "${configuration_resources}"
+}
+
+main
