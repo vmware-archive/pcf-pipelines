c395e2d202aa98a6f7ba9eb1eb062fecc2690fba
Christian Ang <cang@pivotal.io>
2017-11-30 15:18:32 -0800

    Use correct params in configure-product task.sh
    [#153262778]

--- tasks/configure-product/task.sh
+++ tasks/configure-product/task.sh
@@ -9,8 +9,8 @@ function main() {
 
   om-linux \
     --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" \
-    --username "${OPS_MGR_USR}" \
-    --password "${OPS_MGR_PWD}" \
+    --username "${OPSMAN_USERNAME}" \
+    --password "${OPSMAN_PASSWORD}" \
     --skip-ssl-validation \
     configure-product \
     --product-name "${PRODUCT_NAME}" \
