c738e55b2b6defc4ead1f9cd3a273fe9cae5a061
Slawek Ligus <sligus@pivotal.io>
2018-01-11 12:48:38 -0800

    Use - instead of _ in backup-prepare job
    [#154004349]
    
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

--- tasks/config-ert/task.sh
+++ tasks/config-ert/task.sh
@@ -486,7 +486,7 @@ cf_network=$(
 )
 
 JOB_RESOURCE_CONFIG="{
-  \"backup_prepare\": { \"instances\": $BACKUP_PREPARE_INSTANCES },
+  \"backup-prepare\": { \"instances\": $BACKUP_PREPARE_INSTANCES },
   \"clock_global\": { \"instances\": $CLOCK_GLOBAL_INSTANCES },
   \"cloud_controller\": { \"instances\": $CLOUD_CONTROLLER_INSTANCES },
   \"cloud_controller_worker\": { \"instances\": $CLOUD_CONTROLLER_WORKER_INSTANCES },
