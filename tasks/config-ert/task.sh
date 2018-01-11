5a91ff4f74288288240ab624d217948d8cdcffc9
Kira Boyle <kboyle@pivotal.io>
2018-01-11 12:46:44 -0800

    Put job resource config into an argjson in config-ert
    [#154004349]
    
    Signed-off-by: Slawek Ligus <sligus@pivotal.io>

--- tasks/config-ert/task.sh
+++ tasks/config-ert/task.sh
@@ -543,7 +543,7 @@ cf_resources=$(
     --arg mysql_nsx_lb_pool_name "${MYSQL_NSX_LB_POOL_NAME}" \
     --arg mysql_nsx_lb_security_group "${MYSQL_NSX_LB_SECURITY_GROUP}" \
     --arg mysql_nsx_lb_port "${MYSQL_NSX_LB_PORT}" \
-    --arg job_resource_config "${JOB_RESOURCE_CONFIG}" \
+    --argjson job_resource_config "${JOB_RESOURCE_CONFIG}" \
     '
     $job_resource_config
 
