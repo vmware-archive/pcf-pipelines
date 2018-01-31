3605c1d1979fba197a7b6067e7fad8c50b722cd8
ahocking <ahocking@pivotal.io>
2018-01-30 19:31:09 -0700

    Add data element to make sure public ips are created before used

--- install-pcf/azure/terraform/c0-azure-multi-res-group/public_ips.tf
+++ install-pcf/azure/terraform/c0-azure-multi-res-group/public_ips.tf
@@ -9,11 +9,23 @@ resource "azurerm_public_ip" "tcp-lb-public-ip" {
   public_ip_address_allocation = "static"
 }
 
+data "azurerm_public_ip" "tcp-lb-public-ip" {
+  name                = "${var.env_name}-tcp-lb-public-ip"
+  resource_group_name = "${var.azure_multi_resgroup_network}"
+  depends_on          = ["azurerm_public_ip.tcp-lb-public-ip"]
+}
+
 resource "azurerm_public_ip" "web-lb-public-ip" {
   name                         = "${var.env_name}-web-lb-public-ip"
   location                     = "${var.location}"
   resource_group_name          = "${var.azure_multi_resgroup_network}"
   public_ip_address_allocation = "static"
+}
+
+data "azurerm_public_ip" "web-lb-public-ip" {
+  name                = "${var.env_name}-web-lb-public-ip"
+  resource_group_name = "${var.azure_multi_resgroup_network}"
+  depends_on          = ["azurerm_public_ip.web-lb-public-ip"]
 }
 
 resource "azurerm_public_ip" "opsman-public-ip" {
@@ -23,6 +35,12 @@ resource "azurerm_public_ip" "opsman-public-ip" {
   public_ip_address_allocation = "static"
 }
 
+data "azurerm_public_ip" "opsman-public-ip" {
+  name                = "${var.env_name}-opsman-public-ip"
+  resource_group_name = "${var.azure_multi_resgroup_network}"
+  depends_on          = ["azurerm_public_ip.opsman-public-ip"]
+}
+
 resource "azurerm_public_ip" "ssh-proxy-lb-public-ip" {
   name                         = "${var.env_name}-ssh-proxy-lb-public-ip"
   location                     = "${var.location}"
@@ -30,10 +48,21 @@ resource "azurerm_public_ip" "ssh-proxy-lb-public-ip" {
   public_ip_address_allocation = "static"
 }
 
+data "azurerm_public_ip" "ssh-lb-public-ip" {
+  name                = "${var.env_name}-ssh-lb-public-ip"
+  resource_group_name = "${var.azure_multi_resgroup_network}"
+  depends_on          = ["azurerm_public_ip.ssh-lb-public-ip"]
+}
 
 resource "azurerm_public_ip" "jb-lb-public-ip" {
   name                         = "${var.env_name}-jb-lb-public-ip"
   location                     = "${var.location}"
   resource_group_name          = "${var.azure_multi_resgroup_network}"
   public_ip_address_allocation = "static"
+}
+
+data "azurerm_public_ip" "jb-lb-public-ip" {
+  name                = "${var.env_name}-jb-lb-public-ip"
+  resource_group_name = "${var.azure_multi_resgroup_network}"
+  depends_on          = ["azurerm_public_ip.jb-lb-public-ip"]
 }
