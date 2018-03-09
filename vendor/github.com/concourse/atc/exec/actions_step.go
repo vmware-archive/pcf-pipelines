be82616b4be59ed7c9193f8a4bfd3a78242d8ba8
David Timm <dtimm@pivotal.io>
2018-03-09 09:16:45 -0700

    Revendor dependencies for updated version of concourse
    [#155834024]
    
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

--- vendor/github.com/concourse/atc/exec/actions_step.go
+++ vendor/github.com/concourse/atc/exec/actions_step.go
@@ -1,18 +1,18 @@
 package exec
 
 import (
-	"os"
+	"context"
+	"errors"
 
 	"code.cloudfoundry.org/lager"
 
-	"github.com/concourse/atc/resource"
 	"github.com/concourse/atc/worker"
 )
 
 //go:generate counterfeiter . Action
 
 type Action interface {
-	Run(lager.Logger, *worker.ArtifactRepository, <-chan os.Signal, chan<- struct{}) error
+	Run(context.Context, lager.Logger, *worker.ArtifactRepository) error
 	ExitStatus() ExitStatus
 }
 
@@ -55,15 +55,15 @@ func (s ActionsStep) Using(repo *worker.ArtifactRepository) Step {
 // Run will first call Initializing on build events delegate. Then it will call
 // Run on every action. If any action fails it will notify delegate with Failed.
 // It will call ActionCompleted after each action run that succeeds.
-func (s *ActionsStep) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
+func (s *ActionsStep) Run(ctx context.Context) error {
 	succeeded := true
 	for _, action := range s.actions {
-		err := action.Run(s.logger, s.repository, signals, ready)
+		err := action.Run(ctx, s.logger, s.repository)
 		if err != nil {
-			if err == resource.ErrAborted {
-				s.logger.Debug("resource-aborted")
-				s.buildEventsDelegate.Failed(s.logger, ErrInterrupted)
-				return ErrInterrupted
+			if err == context.DeadlineExceeded || err == context.Canceled {
+				// XXX: here for backwards-compatibility. this shouldn't be so coupled.
+				s.buildEventsDelegate.Failed(s.logger, errors.New("interrupted"))
+				return err
 			}
 
 			s.logger.Error("failed-to-run-action", err)
