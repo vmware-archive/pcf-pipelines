3c286c4d798cdc68d0112dc271cf8f6fc98f97e7
Pivotal <pivotal@pivotal.io>
2018-03-19 10:57:57 -0600

    Properly escape the file This reverts commit ed51441e91ec73e4b263381722ebd7e5f40321ac.
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

--- ci/tasks/create-release/task.sh
+++ ci/tasks/create-release/task.sh
@@ -64,25 +64,6 @@ EOF
 
 set -e
 
-# Add a tag to the docker image in each task.yml to enforce version
-files=$(
-  find pcf-pipelines \
-  -type f \
-  -name task.yml |
-  grep -v pcf-pipelines/ci
-)
-
-for f in ${files[@]}; do
-  if [[ $(cat "$f" | yaml_patch_linux -o <(echo "$test_for_docker_image") 2>/dev/null ) ]]; then
-    echo "Using pivnet release for ${f}"
-    cat "$f" |
-    yaml_patch_linux \
-      -o <(echo "$hardcode_rootfs_version") \
-      > "$f".bk
-    mv "$f".bk "$f"
-  fi
-done
-
 # Add a tag to each pipeline.yml to enforce version
 files=$(
   find pcf-pipelines \
@@ -106,6 +87,25 @@ for f in ${files[@]}; do
     # Remove git_private_key from params as it is no longer needed
     params_file=$(dirname "$f")/params.yml
     sed -i -e '/git_private_key:/d' "$params_file"
+  fi
+done
+
+# Add a tag to the docker image in each task.yml to enforce version
+files=$(
+  find pcf-pipelines \
+  -type f \
+  -name task.yml |
+  grep -v pcf-pipelines/ci
+)
+
+for f in ${files[@]}; do
+  if [[ $(cat "$f" | yaml_patch_linux -o <(echo "$test_for_docker_image") 2>/dev/null ) ]]; then
+    echo "Using pivnet release for ${f}"
+    cat "$f" |
+    yaml_patch_linux \
+      -o <(echo "$hardcode_rootfs_version") \
+      > "$f".bk
+    mv "$f".bk "$f"
   fi
 done
 
