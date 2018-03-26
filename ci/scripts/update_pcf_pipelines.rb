3c24abaf9666b37c74fb5d0706e837912eea6855
JT Archie <jarchie@pivotal.io>
2018-03-26 14:07:40 -0600

    use YAML library in Ruby instead of text concatenation
    Signed-off-by: Huan Wang <huwang@pivotal.io>

--- ci/scripts/update_pcf_pipelines.rb
+++ ci/scripts/update_pcf_pipelines.rb
@@ -1,40 +1,39 @@
 #!/usr/bin/env ruby
+
 require 'tempfile'
+require 'yaml'
 
-def write_params(file, prefix, key, note_path)
+@params = {}
+def load_params(key, note_path)
   lpass_path = "Shared-PCF-NORM/#{note_path}"
-  creds = `lpass show #{lpass_path}  --notes`
+  creds = `lpass show #{lpass_path}  --notes`.chomp
 
-  if creds.to_s.strip.empty?
+  if creds.empty?
     puts "Could not fetch creds from #{lpass_path}"
     puts creds
     exit(1)
   end
 
-  file.write("#{prefix}#{key}: |\n")
-
-  creds.each_line do |line|
-    file.write("  #{prefix}#{line}")
-  end
+  @params[key] = creds
 end
 
-fileName = 'pipeline_params'
-tempfile = Tempfile.new(fileName)
 
-prefix = ""
-offline_prefix = "  "
-write_params(tempfile, prefix,'rc_aws_install_params','rc/install-pcf/aws/pipeline.yml')
-write_params(tempfile, prefix,'rc_gcp_install_params' ,'rc/install-pcf/gcp/pipeline.yml')
-write_params(tempfile, prefix,'rc_azure_install_params' ,'rc/install-pcf/azure/pipeline.yml')
-write_params(tempfile, prefix,'rc_vsphere_install_params' ,'rc/install-pcf/vsphere/pipeline.yml')
-write_params(tempfile, offline_prefix, 'rc_offline_vsphere_install_params', 'offline/install-pcf/vsphere/pipeline.yml')
-#write_params $tmpfile "" "create_offline_pinned_pipelines_params" 'create-offline-pinned-pipelines-params'
-#write_params $tmpfile "" "unpack_pcf_pipelines_combined_params" 'unpack-pcf-pipelines-combined-params'
-tempfile.write('  rc_offline_pipeline_name: rc-offline-vsphere-install')
-tempfile.flush
+
+
+load_params('rc_aws_install_params','rc/install-pcf/aws/pipeline.yml')
+load_params('rc_gcp_install_params' ,'rc/install-pcf/gcp/pipeline.yml')
+load_params('rc_azure_install_params' ,'rc/install-pcf/azure/pipeline.yml')
+load_params('rc_vsphere_install_params' ,'rc/install-pcf/vsphere/pipeline.yml')
+load_params('rc_lre_gcp_upgrade_ops_manager_params' ,'lre/upgrade-ops-manager/gcp/pipeline.yml')
+# write_params( 'rc_offline_vsphere_install_params', 'offline/install-pcf/vsphere/pipeline.yml')
+# tempfile.write('  rc_offline_pipeline_name: rc-offline-vsphere-install')
+
+file = Tempfile.new('pipeline_params')
+file.write(@params.to_yaml)
+file.close
 
 flyCmd = "fly -t ci sp -p pcf-pipelines-master -c ci/pcf-pipelines/pipeline.yml \
-  -l #{tempfile.path} \
+  -l #{file.path} \
   -l <(lpass show Shared-PCF-NORM/pcf-pipelines-params --notes) \
   -l <(lpass show Shared-PCF-NORM/pcf-norm-github --notes) \
   -l <(lpass show Shared-PCF-NORM/norm-pivnet --notes) \
@@ -43,5 +42,3 @@ flyCmd = "fly -t ci sp -p pcf-pipelines-master -c ci/pcf-pipelines/pipeline.yml 
 puts flyCmd
 
 exec "bash -c '#{flyCmd}'"
-#  -l <(lpass show czero-pivnet --notes)
-#  -l <(lpass show minio-lrpiec03 --notes)
