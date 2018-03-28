64e6656db22077d5f60e19e4efafaea609869cbb
JT Archie <j.archie@pivotal.io>
2018-03-28 10:43:45 -0600

    Add offline vsphere back to the pipeline
    [#155861558]
    
    Signed-off-by: David Timm <dtimm@pivotal.io>

--- ci/scripts/update_pcf_pipelines.rb
+++ ci/scripts/update_pcf_pipelines.rb
@@ -3,8 +3,8 @@
 require 'tempfile'
 require 'yaml'
 
-@params = {}
-def load_params(key, note_path)
+params = {}
+def load_param(note_path)
   lpass_path = "Shared-PCF-NORM/#{note_path}"
   creds = `lpass show #{lpass_path}  --notes`.chomp
 
@@ -14,23 +14,25 @@ def load_params(key, note_path)
     exit(1)
   end
 
-  @params[key] = creds
+  creds
 end
 
 
 
 
-load_params('rc_aws_install_params','rc/install-pcf/aws/pipeline.yml')
-load_params('rc_gcp_install_params' ,'rc/install-pcf/gcp/pipeline.yml')
-load_params('rc_azure_install_params' ,'rc/install-pcf/azure/pipeline.yml')
-load_params('rc_vsphere_install_params' ,'rc/install-pcf/vsphere/pipeline.yml')
-load_params('rc_lre_gcp_upgrade_ops_manager_params' ,'lre/upgrade-ops-manager/gcp/pipeline.yml')
-load_params('rc_gcp_upgrade_pas_tile_params', 'rc/upgrade-pas-tile/gcp/pipeline.yml')
-# write_params( 'rc_offline_vsphere_install_params', 'offline/install-pcf/vsphere/pipeline.yml')
-# tempfile.write('  rc_offline_pipeline_name: rc-offline-vsphere-install')
+params['rc_aws_install_params'] = load_param('rc/install-pcf/aws/pipeline.yml')
+params['rc_gcp_install_params'] = load_param('rc/install-pcf/gcp/pipeline.yml')
+params['rc_azure_install_params'] = load_param('rc/install-pcf/azure/pipeline.yml')
+params['rc_vsphere_install_params'] = load_param('rc/install-pcf/vsphere/pipeline.yml')
+params['rc_lre_gcp_upgrade_ops_manager_params'] = load_param('lre/upgrade-ops-manager/gcp/pipeline.yml')
+params['rc_gcp_upgrade_pas_tile_params'] = load_param('rc/upgrade-pas-tile/gcp/pipeline.yml')
+params['unpack_pcf_pipelines_combined_params'] = {
+    'rc_offline_vsphere_install_params' => load_param('offline/install-pcf/vsphere/pipeline.yml'),
+    'rc_offline_pipeline_name' => 'rc-offline-vsphere-install'
+}.to_yaml
 
 file = Tempfile.new('pipeline_params')
-file.write(@params.to_yaml)
+file.write(params.to_yaml)
 file.close
 
 flyCmd = "fly -t ci sp -p pcf-pipelines-master -c ci/pcf-pipelines/pipeline.yml \
