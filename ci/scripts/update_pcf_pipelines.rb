c51ac386ad5ad2994d3a486d83ad3e952d8ca0d0
JT Archie <j.archie@pivotal.io>
2018-03-29 09:24:46 -0600

    set script for offline envs
    Signed-off-by: David Timm <dtimm@pivotal.io>

--- ci/scripts/update_pcf_pipelines.rb
+++ ci/scripts/update_pcf_pipelines.rb
@@ -4,6 +4,7 @@ require 'tempfile'
 require 'yaml'
 
 params = {}
+
 def load_param(note_path)
   lpass_path = "Shared-PCF-NORM/#{note_path}"
   creds = `lpass show #{lpass_path}  --notes`.chomp
@@ -18,8 +19,6 @@ def load_param(note_path)
 end
 
 
-
-
 params['rc_aws_install_params'] = load_param('rc/install-pcf/aws/pipeline.yml')
 params['rc_gcp_install_params'] = load_param('rc/install-pcf/gcp/pipeline.yml')
 params['rc_azure_install_params'] = load_param('rc/install-pcf/azure/pipeline.yml')
@@ -29,8 +28,9 @@ params['rc_gcp_upgrade_pas_tile_params'] = load_param('rc/upgrade-pas-tile/gcp/p
 params['create_offline_pinned_pipelines_params'] = load_param('create-offline-pinned-pipelines/pipeline.yml')
 params['unpack_pcf_pipelines_combined_params'] = {
     'rc_offline_vsphere_install_params' => load_param('offline/install-pcf/vsphere/pipeline.yml'),
-    'rc_offline_pipeline_name' => 'rc-offline-vsphere-install'
-}.to_yaml
+    'rc_offline_pipeline_name' => 'rc-offline-vsphere-install',
+
+}.merge(YAML.load(load_param('unpack-pcf-pipelines-combined-params'))).to_yaml
 
 file = Tempfile.new('pipeline_params')
 file.write(params.to_yaml)
@@ -40,8 +40,7 @@ flyCmd = "fly -t ci sp -p pcf-pipelines-master -c ci/pcf-pipelines/pipeline.yml 
   -l #{file.path} \
   -l <(lpass show Shared-PCF-NORM/pcf-pipelines-params --notes) \
   -l <(lpass show Shared-PCF-NORM/pcf-norm-github --notes) \
-  -l <(lpass show Shared-PCF-NORM/norm-pivnet --notes) \
-  -l <(lpass show Shared-PCF-NORM/czero-mineo-lrpiec03 --notes)"
+  -l <(lpass show Shared-PCF-NORM/norm-pivnet --notes)"
 
 puts flyCmd
 
