030403b659ef174dee5f97db8369988904c4caa5
David Timm <dtimm@pivotal.io>
2018-04-06 10:16:58 -0600

    use only one regex in the tests
    [#152937990]
    
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

--- pcf_pipelines_test.go
+++ pcf_pipelines_test.go
@@ -16,7 +16,6 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var oldPlaceholderRegexp = regexp.MustCompile("{{([a-zA-Z0-9-_]+)}}")
 var placeholderRegexp = regexp.MustCompile("[({]{2}([a-zA-Z0-9-_]+)[)}]{2}")
 
 var _ = Describe("pcf-pipelines", func() {
@@ -62,7 +61,7 @@ var _ = Describe("pcf-pipelines", func() {
 
 			It("specifies only valid job names in any `passed` definitions in the buildplan", func() {
 				var config atc.Config
-				cleanConfigBytes := oldPlaceholderRegexp.ReplaceAll(configBytes, []byte("true"))
+				cleanConfigBytes := placeholderRegexp.ReplaceAll(configBytes, []byte("true"))
 				err := yaml.Unmarshal(cleanConfigBytes, &config)
 				Expect(err).NotTo(HaveOccurred())
 
@@ -75,7 +74,7 @@ var _ = Describe("pcf-pipelines", func() {
 
 			It("specifies all and only the params that the pipeline's tasks expect", func() {
 				var config atc.Config
-				cleanConfigBytes := oldPlaceholderRegexp.ReplaceAll(configBytes, []byte("true"))
+				cleanConfigBytes := placeholderRegexp.ReplaceAll(configBytes, []byte("true"))
 				err := yaml.Unmarshal(cleanConfigBytes, &config)
 				Expect(err).NotTo(HaveOccurred())
 
