20ef3f740bdca769dc6f7f1d65b3c91f070ed1ba
Kira Boyle <kboyle@pivotal.io>
2018-04-17 16:13:00 -0600

    Fix the failing wipe-env job
    [#156820288]
    
    Signed-off-by: Huan Wang <huwang@pivotal.io>

--- install-pcf/azure/tasks/wipe-env/task.sh
+++ install-pcf/azure/tasks/wipe-env/task.sh
@@ -17,72 +17,22 @@ function delete-opsman-installation() {
   fi
 }
 
-function delete-opsman() {
-  az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
-  local opsman_vms=$(az vm list -g $AZURE_TERRAFORM_PREFIX | jq -r ".[].name | select(. |startswith(\"$AZURE_TERRAFORM_PREFIX-ops-manager\"))")
-
-  for om_vm_name in $opsman_vms; do
-    echo "Removing $om_vm_name ..."
-    az vm delete --yes --resource-group $AZURE_TERRAFORM_PREFIX --name "$om_vm_name"
-  done
-}
-
 function delete-infrastructure() {
   echo "=============================================================================================="
   echo "Executing Terraform Destroy ...."
   echo "=============================================================================================="
 
-  terraform init "pcf-pipelines/install-pcf/azure/terraform/${AZURE_PCF_TERRAFORM_TEMPLATE}"
+  terraform init "/home/vcap/app/terraforming-azure"
 
   terraform destroy -force \
-    -var "subscription_id=${AZURE_SUBSCRIPTION_ID}" \
-    -var "client_id=${AZURE_CLIENT_ID}" \
-    -var "client_secret=${AZURE_CLIENT_SECRET}" \
-    -var "tenant_id=${AZURE_TENANT_ID}" \
-    -var "location=dontcare" \
-    -var "env_name=dontcare" \
-    -var "env_short_name=dontcare" \
-    -var "azure_terraform_vnet_cidr=dontcare" \
-    -var "azure_terraform_subnet_infra_cidr=dontcare" \
-    -var "azure_terraform_subnet_ert_cidr=dontcare" \
-    -var "azure_terraform_subnet_services1_cidr=dontcare" \
-    -var "azure_terraform_subnet_dynamic_services_cidr=dontcare" \
-    -var "ert_subnet_id=dontcare" \
-    -var "pcf_ert_domain=dontcare" \
-    -var "system_domain=dontcare" \
-    -var "apps_domain=dontcare" \
-    -var "pub_ip_pcf_lb=dontcare" \
-    -var "pub_ip_id_pcf_lb=dontcare" \
-    -var "pub_ip_tcp_lb=dontcare" \
-    -var "pub_ip_id_tcp_lb=dontcare" \
-    -var "priv_ip_mysql_lb=dontcare" \
-    -var "pub_ip_ssh_proxy_lb=dontcare" \
-    -var "pub_ip_id_ssh_proxy_lb=dontcare" \
-    -var "pub_ip_opsman_vm=dontcare" \
-    -var "pub_ip_id_opsman_vm=dontcare" \
-    -var "pub_ip_jumpbox_vm=dontcare" \
-    -var "pub_ip_id_jumpbox_vm=dontcare" \
-    -var "subnet_infra_id=dontcare" \
-    -var "ops_manager_image_uri=dontcare" \
-    -var "vm_admin_username=dontcare" \
-    -var "vm_admin_public_key=dontcare" \
-    -var "azure_multi_resgroup_network=dontcare" \
-    -var "azure_multi_resgroup_pcf=dontcare" \
-    -var "azure_opsman_priv_ip=dontcare" \
-    -var "azure_storage_account_name=dontcare" \
-    -var "azure_buildpacks_container=dontcare" \
-    -var "azure_droplets_container=dontcare" \
-    -var "azure_packages_container=dontcare" \
-    -var "azure_resources_container=dontcare" \
-    -var "om_disk_size_in_gb=50" \
+    -var-file "${ROOT}/terraform-vars/terraform.tfvars" \
     -state "${ROOT}/terraform-state/terraform.tfstate" \
     -state-out "${ROOT}/terraform-state-output/terraform.tfstate" \
-    "pcf-pipelines/install-pcf/azure/terraform/${AZURE_PCF_TERRAFORM_TEMPLATE}"
+    "/home/vcap/app/terraforming-azure"
 }
 
 function main() {
   delete-opsman-installation
-  delete-opsman
   delete-infrastructure
 }
 
