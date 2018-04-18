a18108846143f6ea5fa53860ba7594d964f43637
Huan Wang <huwang@pivotal.io>
2018-04-18 10:25:22 -0600

    remove old terraform tamplates for azure
    [#156820288]
    
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

--- install-pcf/azure/terraform/c0-azure-multi-res-group/constants.tf
+++
@@ -1,9 +0,0 @@
-///////////////////////////////////////////////
-//////// Pivotal Customer[0] //////////////////
-////////                          /////////////
-///////////////////////////////////////////////
-
-// the CPI uses this as a wildcard to stripe disks across multiple storage accounts
-data "template_file" "base_storage_account_wildcard" {
-  template = "boshvms"
-}
