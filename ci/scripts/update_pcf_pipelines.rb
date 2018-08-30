6d80650e1c13c90453d97e65ea8eac1767480056
JT Archie <jarchie@pivotal.io>
2018-08-30 15:59:00 -0600

    set FLY_TARGET to sync up CI pipeline
    Signed-off-by: Huan Wang <huwang@pivotal.io>

--- ci/scripts/update_pcf_pipelines.rb
+++ ci/scripts/update_pcf_pipelines.rb
@@ -28,7 +28,7 @@ file = Tempfile.new('pipeline_params')
 file.write(params.to_yaml)
 file.close
 
-fly_cmd = "fly -t ci sp -p pcf-pipelines-master -c ci/pcf-pipelines/pipeline.yml \
+fly_cmd = "fly -t #{ENV.fetch('FLY_TARGET')} sp -p pcf-pipelines-master -c ci/pcf-pipelines/pipeline.yml \
   -l #{file.path} \
   -l ~/workspace/secrets/pcf-pipelines-params \
   -l ~/workspace/secrets/platform-automation-github \
