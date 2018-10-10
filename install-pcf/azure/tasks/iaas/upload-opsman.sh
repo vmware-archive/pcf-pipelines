58faed959f57f3c8665ac54245ad4338631df131
Andreas Heumaier <developer@andreasheumaier.de>
2018-10-10 21:26:18 +0200

    Update: Adding  configurable opsman image source

--- install-pcf/azure/tasks/iaas/upload-opsman.sh
+++ install-pcf/azure/tasks/iaas/upload-opsman.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ue
 
-current_dir=$( dirname "${BASH_SOURCE[0]}" )
+current_dir=$(dirname "${BASH_SOURCE[0]}")
 source "${current_dir}/parse_opsman_region_url.sh"
 echo "=============================================================================================="
 echo "Getting Azure Ops Manager VHD URI from Pivnet YML...."
 echo "=============================================================================================="
-pcf_opsman_image_vhd=$(parseRegionURL "east_us" "pivnet-opsmgr/*Azure.yml")
+pcf_opsman_image_vhd=$(parseRegionURL "${AZURE_IMAGE_SOURCE_REGION}" "pivnet-opsmgr/*Azure.yml")
 echo "Found Azure OpsMan Image @ $pcf_opsman_image_vhd ...."
-echo $pcf_opsman_image_vhd > opsman-metadata/uri
+echo $pcf_opsman_image_vhd >opsman-metadata/uri
