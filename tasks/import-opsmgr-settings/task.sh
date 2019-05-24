8e8e8ca68df52364bf8aa7f06d3578dbdc96fcde
John Cornish <jcornish@pivotal.io>
2019-05-24 09:33:52 -0600

    change ordering of decryption-passphrase and installation so import-opsman-settings does not fail #384
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

--- tasks/import-opsmgr-settings/task.sh
+++ tasks/import-opsmgr-settings/task.sh
@@ -31,8 +31,8 @@ function main() {
       --skip-ssl-validation \
       --request-timeout 86400 \
       import-installation \
-      --installation "${cwd}/opsmgr-settings/${OPSMAN_SETTINGS_FILENAME}" \
-      --decryption-passphrase "${OPSMAN_PASSPHRASE}"
+      --decryption-passphrase "${OPSMAN_PASSPHRASE}" \
+      --installation "${cwd}/opsmgr-settings/${OPSMAN_SETTINGS_FILENAME}"
  }
 
  main "${PWD}"
